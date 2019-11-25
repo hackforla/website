e6a0649782936d7d6739e8797b40106240ee4758
StephenVNelson <StephenVNelson@gmail.com>
2019-11-24 21:36:00 -0800

    made status selector work individually

--- assets/js/all-projects.js
+++ assets/js/all-projects.js
@@ -2,14 +2,29 @@
 
 var lis = document.getElementById("project-list").getElementsByTagName("li");
 var locationSelector = document.getElementById("location-selection");
+var statusSelector = document.getElementById("status-selection");
 
 locationSelector.addEventListener('change', (event) => {
   var location = event.target.value;
-  console.log(location)
   Array.from(lis).forEach(function(li){
     if (location == "LOCATION"){
       li.style.display = 'block';
     } else if (li.className.includes(location)) {
+      li.style.display = 'block';
+    } else {
+      li.style.display = 'none';
+    }
+  });
+});
+
+
+statusSelector.addEventListener('change', (event) => {
+  var status = event.target.value;
+  console.log(status)
+  Array.from(lis).forEach(function(li){
+    if (status == "STATUS"){
+      li.style.display = 'block';
+    } else if (li.className.includes(status)) {
       li.style.display = 'block';
     } else {
       li.style.display = 'none';
