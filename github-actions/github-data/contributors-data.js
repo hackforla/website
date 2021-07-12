03d0a073246db886ae8a417a779fb274291f7af8
AlexeyS <xengawe@gmail.com>
2021-07-12 13:03:14 -0700

    update contr-dataJS

--- github-actions/github-data/contributors-data.js
+++ github-actions/github-data/contributors-data.js
@@ -1,1 +1,13 @@
-console.log('hello');
+const core = require("@actions/core");
+const { Octokit } = require("@octokit/rest");
+const trueContributorsMixin = require("true-github-contributors");
+const _ = require('lodash');
+
+
+Object.assign(Octokit.prototype, trueContributorsMixin);
+const octokit = new Octokit({ auth: process.env.token });
+
+
+(async function main(){
+  console.log('hello');
+})()
