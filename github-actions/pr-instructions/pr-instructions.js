4f76a367e8ab371dcf04f5617332682a7d079ba7
Aveline-art <avali4907@outlook.com>
2021-07-16 16:50:14 -0700

    corrected error

--- github-actions/pr-instructions/pr-instructions.js
+++ github-actions/pr-instructions/pr-instructions.js
@@ -40,20 +40,8 @@ function createMessage() {
 git pull ${cloneURL} ${nameOfFromBranch}`
 
 
-    const path = './github-actions/pr-instructions'
-    fs.readdir(path, function (err, files) {
-        //handling error
-        if (err) {
-            return console.log('Unable to scan directory: ' + err);
-        }
-        //listing all files using forEach
-        files.forEach(function (file) {
-            // Do whatever you want to do with the file
-            console.log(file);
-        });
-    });
-
-    const text = fs.readFileSync("./github-actions/pr-instructions/pr-instructions-template.md.md").toString('utf-8');
+    const path = './github-actions/pr-instructions/pr-instructions-template.md'
+    const text = fs.readFileSync(path).toString('utf-8');
     const completedInstuctions = text.replace('${commandlineInstructions}', instructionString)
 
     return completedInstuctions
