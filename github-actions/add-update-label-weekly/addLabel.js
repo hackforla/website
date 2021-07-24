844fe9a531fd21c7d893d9a01b4b32f224766d74
Aveline-art <avali4907@outlook.com>
2021-07-24 11:04:44 -0700

    made improvements to behavior

--- github-actions/add-update-label-weekly/addLabel.js
+++ github-actions/add-update-label-weekly/addLabel.js
@@ -4,8 +4,8 @@ const findLinkedIssue = require('../utils/findLinkedIssue');
 // Global variables
 var github;
 var context;
-const removeLabels = 'Status: Updated' // label to remove
-const addLabels = ['To Update !']; // labels to add
+const removeLabels = ['Status: Updated'] // labels to remove
+const addLabels = ['To Update !']; // labels to remove then add
 const updatedByDays = 3; // number of days ago to check for updates
 
 /**
@@ -33,8 +33,8 @@ async function main({ g, c, columnId }) {
 
     // Adds label if the issue's timeline indicates the issue is outdated.
     if (isTimelineOutdated(timeline, num, assignee)) {
-      addUpdateLabel(num)
       console.log(`Going to ask for an update now for issue #${num}`);
+      addUpdateLabel(num);
     } else {
       console.log(`No updates needed for issue #${num}`);
     }
@@ -51,25 +51,29 @@ async function getIssueNumsFromColumn(columnId) {
   let issueNums = [];
   while (page < 100) {
     try {
+      // https://octokit.github.io/rest.js/v18#projects-list-cards
       const results = await github.projects.listCards({
         column_id: columnId,
         per_page: 100,
         page: page
       });
+
+      // Processes results of API call
       if (results.data.length) {
         for (card of results.data) {
           if (card.hasOwnProperty('content_url')) {
-            // Isolates the issue number from the rest of the url
+            // Isolates the issue number from the rest of the url and pushes it into the array.
             const arr = card.content_url.split('/');
-            // Pushes the last item in arr, which is the issue number
             issueNums.push(arr.pop());
           }
         }
       } else {
         break
       }
+
+      // If an error is found, the rest of the script does not stop.
     } catch (err) {
-      throw new Error(err);
+      console.log(err);
     } finally {
       page++
     }
@@ -87,6 +91,7 @@ async function getTimeline(issueNum) {
   let timeline = [];
   while (page < 100) {
     try {
+      // https://octokit.github.io/rest.js/v18#issues-list-events-for-timeline
       const results = await github.issues.listEventsForTimeline({
         owner: context.repo.owner,
         repo: context.repo.repo,
@@ -94,11 +99,15 @@ async function getTimeline(issueNum) {
         per_page: 100,
         page: page,
       });
+
+      // Processes results of API call
       if (results.data.length) {
         timeline.push(...results.data);
       } else {
         break;
       }
+
+      // If an error is found, the rest of the script does not stop.
     } catch (err) {
       console.error(`Could not retrieve timeline for #${issueNum}`);
       break;
@@ -135,30 +144,38 @@ function isTimelineOutdated(timeline, issueNum, assignee) {
  * @param {Number} issueNum an issue's number
  */
 async function addUpdateLabel(issueNum) {
-  try {
-    // https://octokit.github.io/rest.js/v18#issues-remove-label
-    await github.issues.removeLabel({
-      owner: context.repo.owner,
-      repo: context.repo.repo,
-      issue_number: issueNum,
-      name: removeLabels,
-    });
-  } catch (err) {
-    console.error(`No labels to remove for issue #${num}`);
-  } finally {
+
+  const labelsToRemove = removeLabels.concat(addLabels)
+  for (label of labelsToRemove) {
     try {
-      // https://octokit.github.io/rest.js/v18#issues-add-labels
-      await github.issues.addLabels({
+      // https://octokit.github.io/rest.js/v18#issues-remove-label
+      await github.issues.removeLabel({
         owner: context.repo.owner,
         repo: context.repo.repo,
         issue_number: issueNum,
-        labels: addLabels,
+        name: label,
       });
-    } catch {
-      console.error(`Could not add label for issue #${num}`);
-      console.error(err);
+      console.log(`Removed ${label} from issue #${num}`);
+    } catch (err) {
+      console.error(`No ${label} label to remove for issue #${num}`);
     }
   }
+
+  try {
+    // https://octokit.github.io/rest.js/v18#issues-add-labels
+    await github.issues.addLabels({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      issue_number: issueNum,
+      labels: addLabels,
+    });
+
+    // If an error is found, the rest of the script does not stop.
+  } catch {
+    console.error(`Could not add these labels for issue #${num}: ${addLabels}`);
+    console.error(err);
+  }
+
 }
 
 /***********************
