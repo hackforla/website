5d8e90b3e6232803b0a06900b7e342e97c6b102e
macho-catt <cacho.matthew@gmail.com>
2021-08-02 23:46:31 -0700

    Remove unwanted labels

--- github-actions/issue-labels/check-labels.js
+++ github-actions/issue-labels/check-labels.js
@@ -1,9 +1,9 @@
 // Importing modules
-var fs = require("fs");
+var fs = require("fs")
 
 // Global variables
-var github;
-var context;
+var github
+var context
 
 /**
  *
@@ -13,10 +13,10 @@ var context;
  */
 
 function main({ g, c }) {
-  github = g;
-  context = c;
-  const labels = obtainLabels();
-  return checkLabels(labels);
+  github = g
+  context = c
+  const labels = obtainLabels()
+  return checkLabels(labels)
 }
 
 function obtainLabels() {
@@ -25,21 +25,29 @@ function obtainLabels() {
   return labels
 }
 
-function checkLabels(labels) {
+async function checkLabels(labels) {
   const REQUIRED_LABELS = ['Size', 'role', 'Feature']
   const LABEL_MISSING = ['size: missing', 'role missing', 'Feature Missing']
   const issueNum = context.payload.issue.number
-  const owner = context.payload.repository.owner
+  const owner = context.payload.repository.owner.login
   const repo = context.payload.repository.name
   console.log('issue: ', issueNum)
   console.log('owner: ', owner)
   console.log('repo: ', repo)
 
-  /* Ensure that the issue was not already created with labels from LABEL_MISSING array.
-  If so, ignore the label since it's already added
+  /*  Ensure that the issue was not already created with labels from LABEL_MISSING array.
+      If so, remove that label to avoid redundancy
   */
-  const filteredLabels = labels.filter(label => {
-    if (LABEL_MISSING.includes(label) === false) return label
+  const filteredLabels = labels.map(label => {
+    if (LABEL_MISSING.includes(label) === true){
+      await github.issues.removeLabel({
+        owner: owner,
+        repo: repo,
+        issue_number: issueNum,
+        name: label
+      })
+    }
+    return label
   })
 
   console.log(filteredLabels)
