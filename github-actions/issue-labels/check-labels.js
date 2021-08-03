40821f4612d8ed8f3fa22e4994fa4b482e5c15d6
macho-catt <cacho.matthew@gmail.com>
2021-08-02 23:41:25 -0700

    Add more attributes

--- github-actions/issue-labels/check-labels.js
+++ github-actions/issue-labels/check-labels.js
@@ -20,20 +20,24 @@ function main({ g, c }) {
 }
 
 function obtainLabels() {
-  console.log('context:', context)
-  const issueNum = context.payload.issue.number
   const labelsObject = context.payload.issue.labels
   const labels = labelsObject.map(label => label.name)
-  console.log('issue number: ', issueNum)
-  console.log('labels: ', labels)
   return labels
 }
 
 function checkLabels(labels) {
   const REQUIRED_LABELS = ['Size', 'role', 'Feature']
   const LABEL_MISSING = ['size: missing', 'role missing', 'Feature Missing']
+  const issueNum = context.payload.issue.number
+  const owner = context.payload.repository.owner
+  const repo = context.payload.repository.name
+  console.log('issue: ', issueNum)
+  console.log('owner: ', owner)
+  console.log('repo: ', repo)
 
-  // Ensure that the issue was not already created with labels from LABEL_MISSING array
+  /* Ensure that the issue was not already created with labels from LABEL_MISSING array.
+  If so, ignore the label since it's already added
+  */
   const filteredLabels = labels.filter(label => {
     if (LABEL_MISSING.includes(label) === false) return label
   })
