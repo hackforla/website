6475d1e85073a90949866406b0c50b886b69b4aa
macho-catt <cacho.matthew@gmail.com>
2021-08-03 07:40:58 -0700

    Clean function

--- github-actions/issue-labels/check-labels.js
+++ github-actions/issue-labels/check-labels.js
@@ -1,6 +1,10 @@
 // Importing modules
 var fs = require("fs")
 
+// Constant variables
+const REQUIRED_LABELS = ['Size', 'role', 'Feature']
+const LABEL_MISSING = ['size: missing', 'role missing', 'Feature Missing']
+
 // Global variables
 var github
 var context
@@ -16,6 +20,8 @@ function main({ g, c }) {
   github = g
   context = c
   const labels = obtainLabels()
+  const filteredLabels = filterLabels(labels)
+  console.log('Current labels: ', filteredLabels)
   return checkLabels(labels)
 }
 
@@ -26,16 +32,14 @@ function obtainLabels() {
   return labels
 }
 
-async function checkLabels(labels) {
-  const REQUIRED_LABELS = ['Size', 'role', 'Feature']
-  const LABEL_MISSING = ['size: missing', 'role missing', 'Feature Missing']
+/*  Ensure that the issue was not created with labels from LABEL_MISSING array.
+|   If so, remove the label(s) so the script can add them properly later, if needed.
+*/
+async function filterLabels(labels) {
   const issueNum = context.payload.issue.number
   const owner = context.payload.repository.owner.login
   const repo = context.payload.repository.name
 
-  /*  Ensure that the issue was not created with labels from LABEL_MISSING array.
-      If so, remove the label(s) so the script can add them properly later, if needed.
-  */
   const mappedLabels = await Promise.all(labels.map(async (label) => {
     if (LABEL_MISSING.includes(label) === true){
       console.log(`Detected unwanted label: ${label}. Removing...`)
@@ -52,9 +56,14 @@ async function checkLabels(labels) {
   }))
 
   // After removing the unwanted labels, remove them from the array
-  const filteredLabels = mappedLabels.filter(label => label !== undefined)
+  return mappedLabels.filter(label => label !== undefined)
+}
 
-  console.log('Current labels: ', filteredLabels)
+  //todo: regex to check each label and ensure that the proper labels are there
+
+  //if labels are not there, add labels labelled missing
+async function checkLabels(labels) {
+  console.log('here')
 }
 
 module.exports = main
