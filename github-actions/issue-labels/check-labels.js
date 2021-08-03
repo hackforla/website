309375cb63df345fd7322108b3a6571bcef13b8e
macho-catt <cacho.matthew@gmail.com>
2021-08-02 23:02:21 -0700

    Check labels on issue

--- github-actions/issue-labels/check-labels.js
+++ github-actions/issue-labels/check-labels.js
@@ -15,17 +15,30 @@ var context;
 function main({ g, c }) {
   github = g;
   context = c;
-  return checkLabels();
+  const labels = obtainLabels();
+  return checkLabels(labels);
 }
 
-async function checkLabels() {
+function obtainLabels() {
   console.log('context:', context)
   const issueNum = context.payload.issue.number
   const labelsObject = context.payload.issue.labels
   const labels = labelsObject.map(label => label.name)
   console.log('issue number: ', issueNum)
   console.log('labels: ', labels)
-  console.log('####')
+  return labels
+}
+
+function checkLabels(labels) {
+  const REQUIRED_LABELS = ['Size', 'role', 'Feature']
+  const LABEL_MISSING = ['size: missing', 'role missing', 'Feature Missing']
+
+  // Ensure that the issue was not already created with labels from LABEL_MISSING array
+  const filteredLabels = labels.filter(label => {
+    if (LABEL_MISSING.includes(label) === false) return label
+  })
+
+  console.log(filteredLabels)
 }
 
 module.exports = main
