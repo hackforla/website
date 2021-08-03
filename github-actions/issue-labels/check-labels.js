3eb6c9d53c655559d3d619d62324a5a213d6f2f2
macho-catt <cacho.matthew@gmail.com>
2021-08-02 22:18:27 -0700

    Add payload

--- github-actions/issue-labels/check-labels.js
+++ github-actions/issue-labels/check-labels.js
@@ -22,8 +22,8 @@ async function checkLabels() {
   console.log('context:', context)
   const issueNum = context.payload.issue.number
   const labels = context.payload.issue.labels
-  const owner = context.repository.owner
-  const repo = context.repository.full_name
+  const owner = context.payload.repository.owner
+  const repo = context.payload.repository.full_name
   console.log(issueNum)
   console.log(labels)
   console.log(labels[0])
