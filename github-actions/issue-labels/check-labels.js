d4fc0440e199032f8835ea1926db00ded6905847
macho-catt <cacho.matthew@gmail.com>
2021-08-03 16:58:48 -0700

    Clean up code

--- github-actions/issue-labels/check-labels.js
+++ github-actions/issue-labels/check-labels.js
@@ -1,6 +1,3 @@
-// Importing modules
-var fs = require("fs")
-
 // Constant variables
 const REQUIRED_LABELS = ['Size', 'role', 'Feature']
 const LABEL_MISSING = ['size: missing', 'role missing', 'Feature Missing']
@@ -21,8 +18,6 @@ async function main({ g, c }) {
   context = c
   const labels = obtainLabels()
   const filteredLabels = await filterLabels(labels)
-  console.log('Current labels: ', filteredLabels)
-
   const labelsToAdd = checkLabels(filteredLabels)
   console.log('Labels to add: ', labelsToAdd)
 
@@ -40,36 +35,10 @@ function obtainLabels() {
 
 /*
 Ensure that the issue was not created with labels from LABEL_MISSING array.
-If so, remove the label(s) so the script can add them properly later, if needed.
+If so, they will be filtered and dealt with under the addLabels function
 */
 async function filterLabels(labels) {
-  const issueNum = context.payload.issue.number
-  const owner = context.payload.repository.owner.login
-  const repo = context.payload.repository.name
-/*   const mappedLabels = await Promise.all(labels.map(async (label) => {
-    if (LABEL_MISSING.includes(label) === true){
-      console.log(`Detected unwanted label: ${label}. Removing...`)
-
-      try{
-        await github.issues.removeLabel({
-          owner: owner,
-          repo: repo,
-          issue_number: issueNum,
-          name: label
-        })
-      }
-      catch(err) {
-        console.log('Error editing labels: ', err)
-      }
-    }
-    else{
-      return label
-    }
-  })) */
   return labels.filter(label => LABEL_MISSING.includes(label) === false)
-
-  // After removing the unwanted labels, remove them from the array
-  //return mappedLabels.filter(label => label !== undefined)
 }
 
 // Check for missing labels
@@ -88,16 +57,18 @@ function checkLabels(labels) {
   return labelsToAdd
 }
 
-// Add missing labels
+// Add missing labels and remove labels under LABEL_MISSING if necessary
 async function addLabels(labelsToAdd, currentLabels) {
   const issueNum = context.payload.issue.number
   const owner = context.payload.repository.owner.login
   const repo = context.payload.repository.name
+
+  // Use a flattened set to ensure each label is unique
   const labels = [...new Set([
     ...labelsToAdd,
     ...currentLabels
   ])]
-  console.log('flattened set: ', labels)
+
   try {
     const results = await github.issues.setLabels({
       owner: owner,
