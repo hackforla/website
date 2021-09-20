edd05bb14b2dc26e2e36d49c8bf5820c6c165ac6
blakes24 <blakes24@gmail.com>
2021-09-20 14:51:15 -0700

    Update label hierarchy

--- github-actions/sort-closed-issues.js
+++ github-actions/sort-closed-issues.js
@@ -4,23 +4,41 @@
  * @returns - returns an object with the action's result, which is passed on to the next action
  */
 function main({ context }) {
-  const doneLabels = [
-    "role: back end/devOps",
-    "Feature: Analytics",
+  const hardLabels = [
     "Feature: Refactor CSS",
     "Feature: Refactor HTML",
     "Feature: Refactor JS / Liquid",
     "Feature: Refactor GHA",
   ];
 
-  const labels = obtainLabels(context);
+  const softLabels = [
+    "role: back end/devOps",
+    "Feature: Analytics",
+  ];
 
-  for (let label of labels) {
-    /** if issue includes one of the doneLabels there should be no visual changes and it is moved to the Done column */
-    if (doneLabels.includes(label)) {
-      return "Done";
-    }
+  const overrideSoftLabels = ["role: front end"]
+
+  const issueLabels = obtainLabels(context);
+
+  const includesHardLabel = item => hardLabels.includes(item);
+  const includesSoftLabel = item => softLabels.includes(item);
+  const includesOverrideLabel = item => overrideSoftLabels.includes(item);
+
+  /** if issue includes hard labels there should be no visual changes - move to the Done column */
+  if (issueLabels.some(includesHardLabel)) {
+    return "Done";
   }
+
+  /** if issue does not include a hard label, but does contain an override label - move to UAT */
+  if (issueLabels.some(includesOverrideLabel)) {
+    return "UAT";
+  }
+
+  /** if issue includes soft labels (no hard or override) - move to Done */
+  if (issueLabels.some(includesSoftLabel)) {
+    return "Done";
+  }
+
   // all other issues go to UAT column
   return "UAT";
 }
