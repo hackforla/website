fb6b1be800e43c0a117065ec0344766fffacdbc6
utkarshsaboo <ussabooutkarsh@gmail.com>
2022-04-06 08:16:37 -0700

    resolving comments by Tamara, Luke and Saumil

--- github-actions/trigger-issue/add-preliminary-comment/check-label-preliminary-update.js
+++ github-actions/trigger-issue/add-preliminary-comment/check-label-preliminary-update.js
@@ -3,10 +3,10 @@ var github
 var context
 
 /**
+ * @description - entry point of the whole javascript file, finds out whether we need to post the comment or not and returns in the boolean variable shouldpost.
  * @param {Object} github - github object
  * @param {Object} context - context object
  * @returns {object} - An object deciding whether we need to post the comment in the next action or not
- * @description - entry point of the whole javascript file, finds out whether we need to post the comment or not and returns in the boolean variable shouldpost.
  */
 
 function main({g, c}){
@@ -15,17 +15,16 @@ function main({g, c}){
     const issueNum = context.payload.issue.number
     //Find out what the existing labels in the issue are:-
     var existingLabels = obtainLabels()
-
+    
     //With the existing labels we see if we are to post the comment or not(based on whether there exists a relevant role tag or not) and return it as a boolean
     var shouldPost = postComment(existingLabels)
 
     return({shouldPost,issueNum})
 }
 
-
 /**
- * @returns {array} - return an array of just label names
  * @description - this function gets the current label names and returns it in the array nameOfCurrentLabels
+ * @returns {array} - return an array of just label names
  */
 
 function obtainLabels(){
@@ -37,14 +36,14 @@ function obtainLabels(){
 
 
 /**
+ * @description - this function returns a boolean depending on whether the existing labels contain a relevant role tag 
  * @param {array} existingLabels - takes in as an argument the array of role tags returned by obtainLabels function
  * @returns - A boolean which tells whether we are supposed to post a preliminary update based on the given issue checks
- * @description - this function returns a boolean depending on whether the existing labels contain a relevant role tag 
  */
 
 function postComment(existingLabels){
     //issue states that we are to post the comment if--> there is a role: back end/devOps tag...(continued on next comment)
-    if(existingLabels.includes("role: back end/devOps" )){
+    if(existingLabels.includes("role: back end/devOps")){
         return true
     }
 
@@ -54,7 +53,7 @@ function postComment(existingLabels){
     }
 
     //or if there is a role: design tag
-    else if(existingLabels.includes("role: design" )){
+    else if(existingLabels.includes("role: design")){
         return true
     }
 
--- github-actions/trigger-issue/add-preliminary-comment/preliminary-update-comment.js
+++ github-actions/trigger-issue/add-preliminary-comment/preliminary-update-comment.js
@@ -5,61 +5,61 @@ var github
 var context
 
 /**
+ * @description - This function is the entry point into the javascript file, it formats the md file based on the result of the previous step and then posts it to the issue
  * @param {Object} g - github object  
  * @param {Object} c - context object 
  * @param {Boolean} actionResult - the previous gh-action's result
  * @param {Number} issueNum - the number of the issue where the post will be made 
- * @description - This function is the entry point into the javascript file, it formats the md file based on the result of the previous step and then posts it to the issue
  */
 
 async function main({ g, c }, { shouldPost, issueNum }){
-    github = g
-    context = c
-    // If the previous action returns a false, stop here
-    if(shouldPost === false){
-      console.log('No need to post comment.')
-      return
-    }
-    //Else we make the comment with the issuecreator's github handle instead of the placeholder.
-    else{
-      const instructions = makeComment()
-      if(instructions !== null){
+  github = g
+  context = c
+  // If the previous action returns a false, stop here
+  if(shouldPost === false){
+    console.log('No need to post comment.')
+    return
+  }
+  //Else we make the comment with the issuecreator's github handle instead of the placeholder.
+  else{
+    const instructions = makeComment()
+    if(instructions !== null){
       // the actual creation of the comment in github
       await postComment(issueNum, instructions)
     }
-    }
+  }
 }
 
 /**
- * @returns {string} - Comment to be posted with the issue creator's name in it!!!
  * @description - This function makes the comment with the issue assignee's github handle using the raw preliminary.md file
+ * @returns {string} - Comment to be posted with the issue creator's name in it!!!
  */
 
 function makeComment(){
-    // Setting all the variables which formatcomment is to be called with
-    const issueCreator = context.payload.issue.user.login
+  // Setting all the variables which formatcomment is to be called with
+  const issueAssignee = context.payload.issue.user.login
 
-    const commentObject = {
-        replacementString: issueCreator,
-        placeholderString: '${issueCreator}',
-        filePathToFormat: './github-actions/trigger-issue/add-preliminary-comment/preliminary-update.md',
-        textToFormat: null
-    }
+  const commentObject = {
+    replacementString: issueAssignee,
+    placeholderString: '${issueAssignee}',
+    filePathToFormat: './github-actions/trigger-issue/add-preliminary-comment/preliminary-update.md',
+    textToFormat: null
+  }
 
-    // creating the comment with issue creator's name and returning it!
-    const commentWithIssueCreator = formatComment(commentObject)
+  // creating the comment with issue creator's name and returning it!
+  const commentWithIssueCreator = formatComment(commentObject)
 
-    return commentWithIssueCreator
+  return commentWithIssueCreator
 }
 
 
 /**
+ * @description - This function is called by makeComment() and it formats the comment to be posted based on an object input.
  * @param {String} replacementString - the string to replace the placeholder in the md file
  * @param {String} placeholderString - the placeholder to be replaced in the md file
  * @param {String} filePathToFormat - the path of the md file to be formatted
  * @param {String} textToFormat - the text to be formatted. If null, use the md file provided in the path. If provided, format that text
  * @returns {String} - returns a formatted comment to be posted on github
- * @description - This function is called by makeComment() and it formats the comment to be posted based on an object input.
  */
 
 function formatComment({ replacementString, placeholderString, filePathToFormat, textToFormat }){
@@ -70,12 +70,12 @@ function formatComment({ replacementString, placeholderString, filePathToFormat,
 
 
 /**
+ * @description - this function is called by main() with the result of makeComment() as the comment argument and it does the actual posting of the comment.
  * @param {Number} issueNum - the issue number where the comment should be posted
  * @param {String} comment - the comment to be posted
- * @description - this function is called by main() with the result of makeComment() as the comment argument and it does the actual posting of the comment.
  */
 
- async function postComment(issueNum, comment){
+async function postComment(issueNum, comment){
   try{
     await github.issues.createComment({
       owner: context.repo.owner,
