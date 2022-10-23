d5c924ce84eb8a39a5ac38faa768128c5bde6822
Suman2795 <new.suman7070@gmail.com>
2022-10-23 19:40:47 +0530

    made improvements to the 4 catch statements in add-label.js #2438

--- github-actions/add-update-label-weekly/add-label.js
+++ github-actions/add-update-label-weekly/add-label.js
@@ -211,7 +211,7 @@ async function removeLabels(issueNum, ...labels) {
       });
       console.log(`Removed "${label}" from issue #${issueNum}`);
     } catch (err) { 
-      console.error(`Function failed to remove labels.Please refer to the error below: \n ${err}`);
+      console.error(`Function failed to remove labels.Please refer to the error below: \n`,err);
     }
   }
 }
@@ -232,7 +232,7 @@ async function addLabels(issueNum, ...labels) {
     console.log(`Added these labels to issue #${issueNum}: ${labels}`);
     // If an error is found, the rest of the script does not stop.
   } catch {
-    console.error(`Function failed to add labels.Please refer to the error below:\n ${err}`);
+    console.error(`Function failed to add labels.Please refer to the error below:\n`,err);
   }
 }
 async function postComment(issueNum, assignees) {
@@ -246,7 +246,7 @@ async function postComment(issueNum, assignees) {
       body: instructions,
     });
   } catch (err) {
-    console.error(`Function failed to post comments.Please refer to the error below: \n ${err}`);
+    console.error(`Function failed to post comments.Please refer to the error below: \n `,err);
   }
 }
 /***********************
@@ -280,7 +280,7 @@ async function getAssignees(issueNum) {
     assigneesLogins = filterForAssigneesLogins(assigneesData);
     return assigneesLogins
   } catch (err) {
-    console.error(`Function failed to get assignees.Please refer to the error below: \n ${err}`)
+    console.error(`Function failed to get assignees.Please refer to the error below: \n `,err);
     return null
   }
 }
