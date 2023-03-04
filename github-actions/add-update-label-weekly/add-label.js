4247125c34355796de1ddd189eaeec554323cc32
bzzz-coding <86077274+bzzz-coding@users.noreply.github.com>
2023-03-04 12:17:41 -0800

    Updated comments

--- github-actions/add-update-label-weekly/add-label.js
+++ github-actions/add-update-label-weekly/add-label.js
@@ -144,13 +144,14 @@ async function getTimeline(issueNum) {
  */
 
 // assignees is an arrays of `login`'s
+/* I removed the async keyword for this function because there is no async function calls in this function - Bitian Zhang 3/4/23 */
 function isTimelineOutdated(timeline, issueNum, assignees) {
   // Setting initial responseObj and properties to update/refer to these values in the for...loop 
   let responseObj = {
       result: true,
       labels: inactiveLabel,
       }
-  /* I don't understand why this needs to be an await for...loop (I can't reason why isTimelineOutdated should be an async function either) - Bitian Zhang 3/3/23 */
+  /* I don't think this needs to be an await for...loop because there is no async function calls within the loop - Bitian Zhang 3/4/23 */
 	for (let moment of timeline) {
     let eventTimestamp = moment.updated_at ? moment.updated_at : moment.created_at;
 		if (isMomentRecent(eventTimestamp, sevenDayCutoffTime)) { // if event occurred within 7 days
