963c085088ff525c336c7595423706c8419909d4
Ashelle Cancio <vermillion707@gmail.com>
2023-04-12 20:53:01 -0700

    created a parent function to the request function. #1940

--- github-actions/trigger-issue/append-top-comment.js
+++ github-actions/trigger-issue/append-top-comment.js
@@ -5,27 +5,33 @@ var context
 
 /**
  * @description
- * @param {Object} github
- * @param {Object} context
+ * @param {Object} g
+ * @param {Object} c
  */
+async function appendTopComment({g, c}) {
+	github = g
+	context = c
 
-const octokit = new Octokit({ auth: "YOUR-TOKEN" })
-const issueNum = context.payload.issue.number
-const wikiLink =
-  "https://github.com/hackforla/website/wiki/How-to-work-off-of-a-feature-branch";
+	const octokit = new Octokit({ auth: process.env.HFLA_PROJECT_BOARD_TOKEN })
+	const issueNum = context.payload.issue.number
+	const wikiLink =
+		"https://github.com/hackforla/website/wiki/How-to-work-off-of-a-feature-branch";
+	
+	await octokit.request("PATCH /repos/{owner}/{repo}/issues/{issue_number}", {
+		owner: "hackforla",
+		repo: "website",
+		issue_number: issueNum,
+		title: "Instructions for Feature Branch",
+		body: `This issue involves working off of a feature branch. 
+		
+		Check out this [wiki](${wikiLink}) and follow the outlined steps.
+		
+		Note: Do not manually remove this instruction at the bottom.`,
+		state: "open",
+		headers: {
+			"X-GitHub-Api-Version": "2022-11-28"
+		}
+	});
+}
 
-await octokit.request("PATCH /repos/{owner}/{repo}/issues/{issue_number}", {
-  owner: "hackforla",
-  repo: "website",
-  issue_number: issueNum,
-  title: "Instructions for Feature Branch",
-  body: `This issue involves working off of a feature branch. 
-	
-	Check out this [wiki](${wikiLink}) and follow the outlined steps.
-	
-	Note: Do not manually remove the preceding instructions.`,
-  state: "open",
-  headers: {
-    "X-GitHub-Api-Version": "2022-11-28"
-  }
-});
+module.exports = appendTopComment;
