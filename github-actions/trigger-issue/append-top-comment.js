3d5f554ae6b64706122f6b702decef70ea446f32
Ashelle Cancio <vermillion707@gmail.com>
2023-04-12 20:33:51 -0700

    modified body of patch request with relevant data. #1940

--- github-actions/trigger-issue/append-top-comment.js
+++ github-actions/trigger-issue/append-top-comment.js
@@ -1,22 +1,29 @@
-// Octokit.js
-// https://github.com/octokit/core.js#readme
 const {Octokit} = require('@octokit/rest');
+
+var github
+var context
+
 /**
  * @description
  * @param {Object} github
  * @param {Object} context
  */
 
-const octokit = new Octokit({
-  auth: "YOUR-TOKEN"
-});
+const octokit = new Octokit({ auth: "YOUR-TOKEN" })
+const issueNum = context.payload.issue.number
+const wikiLink =
+  "https://github.com/hackforla/website/wiki/How-to-work-off-of-a-feature-branch";
 
 await octokit.request("PATCH /repos/{owner}/{repo}/issues/{issue_number}", {
-  owner: "OWNER",
-  repo: "REPO",
-  issue_number: "ISSUE_NUMBER",
-  title: "Found a bug",
-  body: "I'm having a problem with this.",
+  owner: "hackforla",
+  repo: "website",
+  issue_number: issueNum,
+  title: "Instructions for Feature Branch",
+  body: `This issue involves working off of a feature branch. 
+	
+	Check out this [wiki](${wikiLink}) and follow the outlined steps.
+	
+	Note: Do not manually remove the preceding instructions.`,
   state: "open",
   headers: {
     "X-GitHub-Api-Version": "2022-11-28"
