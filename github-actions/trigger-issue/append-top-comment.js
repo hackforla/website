5fed2d127d9b9fb2197769bcbea70eed77fdcaef
Ashelle Cancio <vermillion707@gmail.com>
2023-04-14 20:21:56 -0700

    refactored lines in file. #1940

--- github-actions/trigger-issue/append-top-comment.js
+++ github-actions/trigger-issue/append-top-comment.js
@@ -1,37 +1,42 @@
-const {Octokit} = require('@octokit/rest');
+const { Octokit } = require("@octokit/rest");
 
-var github
-var context
+var context;
 
 /**
- * @description
+ * @description This function is the entry point into the javascript file, it appends text to an issue
  * @param {Object} g github object
  * @param {Object} c context object
  */
-async function main({g, c}) {
-	github = g
-	context = c
+async function main({ g, c }) {
+  github = g;
+  context = c;
 
-	const octokit = new Octokit({ auth: process.env.HFLA_PROJECT_BOARD_TOKEN })
-	const issueNum = context.payload.issue.number
-	const wikiLink =
-		"https://github.com/hackforla/website/wiki/How-to-work-off-of-a-feature-branch";
-	
-	await octokit.request("PATCH /repos/{owner}/{repo}/issues/{issue_number}", {
-		owner: "hackforla",
-		repo: "website",
-		issue_number: issueNum,
-		title: "Instructions for Feature Branch",
-		body: `This issue involves working off of a feature branch. 
-		
-		Check out this [wiki](${wikiLink}) and follow the outlined steps.
-		
-		Note: Do not manually remove this instruction at the bottom.`,
-		state: "open",
-		headers: {
-			"X-GitHub-Api-Version": "2022-11-28"
-		}
-	});
+  const octokit = new Octokit({ auth: process.env.REPO_TOKEN });
+  const issueNum = context.payload.issue.number;
+	const owner = context.repo.owner;
+	const repo = context.repo.repo;
+  const wikiLink =
+    "https://github.com/hackforla/website/wiki/How-to-work-off-of-a-feature-branch";
+
+  try {
+    await octokit.request("PATCH /repos/{owner}/{repo}/issues/{issue_number}", {
+      owner: owner,
+      repo: repo,
+      issue_number: issueNum,
+      title: "Instructions for Feature Branch",
+      body: `This issue involves working off of a feature branch. 
+			
+			Check out this [wiki](${wikiLink}) and follow the outlined steps.
+			
+			Note: Do not manually remove this instruction at the bottom.`,
+      state: "open",
+      headers: {
+        "X-GitHub-Api-Version": "2022-11-28"
+      }
+    });
+  } catch (err) {
+    throw new Error(err);
+  }
 }
 
-module.exports =  main;
+module.exports = main;
