b98688154fe53d5887d4577a0631b9567a202502
kwangric <43795498+kwangric@users.noreply.github.com>
2023-05-09 15:13:18 -0400

    Extract categories from guide pages

--- assets/js/toolkit.js
+++ assets/js/toolkit.js
@@ -1,47 +1,93 @@
+---
+
+---
+
 // run hashFilter() as soon as page loads to show filtered results based on URL alone, without clicking on navbar
-document.addEventListener("DOMContentLoaded", hashFilter)
+// document.addEventListener("DOMContentLoaded", hashFilter)
 
-//add listener for click in banner, and toggle classes for filters.
-document.querySelectorAll(".toolkit-header__banner-item").forEach(filter => {
-    filter.addEventListener('click', function (event) {
-        let thisCategory = event.currentTarget;
-        let otherCategories = event.currentTarget.parentElement.children;
+// //add listener for click in banner, and toggle classes for filters.
+// document.querySelectorAll(".toolkit-header__banner-item").forEach(filter => {
+//     filter.addEventListener('click', function (event) {
+//         let thisCategory = event.currentTarget;
+//         let otherCategories = event.currentTarget.parentElement.children;
 
-        if (thisCategory.classList.contains('selected-category')) {
-            return;
-        } else {
-            Array.from(otherCategories).forEach((category) => {
-                if (category.classList.contains('selected-category')) {
-                    category.classList.remove('selected-category');
-                    return;
-                }
-            });
+//         if (thisCategory.classList.contains('selected-category')) {
+//             return;
+//         } else {
+//             Array.from(otherCategories).forEach((category) => {
+//                 if (category.classList.contains('selected-category')) {
+//                     category.classList.remove('selected-category');
+//                     return;
+//                 }
+//             });
 
-            thisCategory.classList.toggle('selected-category');
+//             thisCategory.classList.toggle('selected-category');
+//         }
+//     });
+// });
+
+// //add handler for dropdown navigation selection
+// document.getElementById("dropdown-select").onchange = function() {
+//     window.location.href = this.value;
+// }
+
+// //add listener for url change and toggle visible cards.
+// window.addEventListener("hashchange", hashFilter);
+
+// function hashFilter(e) {
+//     let currentHash = location.hash.split('#')[1].replace("+", "").toLowerCase();
+
+//     let cardContainers = document.querySelectorAll("[data-article-type]");
+//     let guidesCategories = ["all", "development", "design", "projectmanagement", "professionaldevelopment"]
+//     if(guidesCategories.includes(currentHash)){
+//         cardContainers.forEach((card) => {
+//             if (currentHash == 'all') {
+//                 card.style.display = 'block';
+//                 return;
+//             }
+//             card.dataset.articleType == currentHash ? card.style.display = "block" : card.style.display = "none";
+//         });
+//     }
+// }
+
+function retrieveFilterCategories(){
+    {% assign projects = site.guide-pages | where: "display", "true" %}
+    let projects = JSON.parse(decodeURIComponent("{{ projects | jsonify | uri_escape }}"));
+
+    const practiceAreas = []
+    const projectStatus = []
+    const projectTools = []
+    const projectSource = []
+    const projectContributors = []
+
+    // Extract categories from guides
+    for (let i = 0; i < projects.length; i++) {
+        const project = projects[i]
+        if (project["practice-area"] && !practiceAreas.includes(project["practice-area"])) {
+            practiceAreas.push(project["practice-area"])
         }
-    });
-});
-
-//add handler for dropdown navigation selection
-document.getElementById("dropdown-select").onchange = function() {
-    window.location.href = this.value;
-}
-
-//add listener for url change and toggle visible cards.
-window.addEventListener("hashchange", hashFilter);
-
-function hashFilter(e) {
-    let currentHash = location.hash.split('#')[1].replace("+", "").toLowerCase();
-
-    let cardContainers = document.querySelectorAll("[data-article-type]");
-    let guidesCategories = ["all", "development", "design", "projectmanagement", "professionaldevelopment"]
-    if(guidesCategories.includes(currentHash)){
-        cardContainers.forEach((card) => {
-            if (currentHash == 'all') {
-                card.style.display = 'block';
-                return;
+        if (project["status"] && !projectStatus.includes(project["status"])) {
+            projectStatus.push(project["status"])
+        }
+        if (project["tools"]) {
+            const tools = project["tools"]
+            for (let j = 0; j < tools.length; j++) {
+                if (!projectTools.includes(tools[j]) && tools[j]) {
+                    projectTools.push(tools[j])
+                }
             }
-            card.dataset.articleType == currentHash ? card.style.display = "block" : card.style.display = "none";
-        });
+        }
+        if (project["source"] && !projectSource.includes(project["source"])) {
+            projectSource.push(project["source"])
+        }
+        if (project["contributors"]) {
+            const contributors = project["contributors"]
+            for (let j = 0; j < contributors.length; j++) {
+                if (!projectContributors.includes(contributors[j]) && contributors[j]) {
+                    projectContributors.push(contributors[j])
+                }
+            }
+        }
     }
+    return {practiceAreas, projectStatus, projectTools, projectSource, projectContributors}
 }
