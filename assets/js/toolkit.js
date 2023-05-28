9a363a6018d4f058f9c34031bf85446193ea529e
kwangric <43795498+kwangric@users.noreply.github.com>
2023-05-27 20:55:11 -0400

    Add collapsable arrows

--- assets/js/toolkit.js
+++ assets/js/toolkit.js
@@ -115,6 +115,14 @@ document.addEventListener("DOMContentLoaded", () => {
             applyFilters(currentFilters)
         });
     });
+
+    // Event listener on arrows to collapse categories
+    const labelArrows = document.querySelectorAll(".labelArrow")
+    labelArrows.forEach(label => {
+        label.addEventListener('click', function() {
+            label.parentElement.classList.toggle("show-none")
+        })
+    })
 })
 
 // Retrieve filter categories from guide pages
@@ -168,7 +176,7 @@ function retrieveFilterCategories() {
 function dropDownFilterComponent(categoryName,filterArray,filterTitle) {
     return `
     <li class='filter-item'>
-    <a class='category-title filter-categories' style='text-transform: capitalize; color: white'>
+    <a class='category-title filter-categories' name='${categoryName}' style='text-transform: capitalize; color: white'>
         ${filterTitle}
         <span id='counter_${categoryName}' class='number-of-checked-boxes'></span>
         <span class='labelArrow' tabindex="0" role="button" aria-label="Toggle Show ${filterTitle} Filters"> ∟ </span>
