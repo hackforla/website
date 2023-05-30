4ed2a6dd830b1ac93a2aae890ae336be1f132e8e
Philip Dimaano <pdimaano7@gmail.com>
2023-05-30 13:03:17 -0700

    Corrected indentation for code blocks following conditional statement

--- assets/js/wins.js
+++ assets/js/wins.js
@@ -108,14 +108,14 @@
 			if (key === '') {
 				continue
 			}
-				let cloneFilterTemplate = filterTemplate.content.firstElementChild.cloneNode(true);
-				cloneFilterTemplate.querySelector("input").value = `role_${key}`;
-					cloneFilterTemplate.querySelector("input").id = `role_${key.replace(/\s+/g, '')}`;
-					cloneFilterTemplate.querySelector("input").name = `role_${key.replace(/\s+/g, '')}`;
-				cloneFilterTemplate.querySelector("input").addEventListener("click",checkboxClickHandler);
-					cloneFilterTemplate.querySelector("label").textContent = `${key}`;
-				cloneFilterTemplate.querySelector("label").htmlFor =`role_${key.replace(/\s+/g, '')}`;
-				roleDropDown.append(cloneFilterTemplate);
+			let cloneFilterTemplate = filterTemplate.content.firstElementChild.cloneNode(true);
+			cloneFilterTemplate.querySelector("input").value = `role_${key}`;
+			cloneFilterTemplate.querySelector("input").id = `role_${key.replace(/\s+/g, '')}`;
+			cloneFilterTemplate.querySelector("input").name = `role_${key.replace(/\s+/g, '')}`;
+			cloneFilterTemplate.querySelector("input").addEventListener("click",checkboxClickHandler);
+			cloneFilterTemplate.querySelector("label").textContent = `${key}`;
+			cloneFilterTemplate.querySelector("label").htmlFor =`role_${key.replace(/\s+/g, '')}`;
+			roleDropDown.append(cloneFilterTemplate);
   	}
   	for(const [key,value] of Object.entries(teamHash) ){
   		let cloneFilterTemplate = filterTemplate.content.firstElementChild.cloneNode(true);
