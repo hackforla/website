66d2bee512a50a4c95463d02b4f60816b695c6ff
Will Gillis <40799239+t-will-gillis@users.noreply.github.com>
2023-07-17 11:22:15 -0700

    Updates issue trigger 4933 (#4936)
    * Update issue-trigger.yml
    
    Corrected GitHub token name,
    Changed actions/github-script@4 to @6
    
    * Update check-labels.js
    
    edited to `github.rest.issues` line 99
    
    * Update post-labels-comment.js
    
    edited to `github.rest.issues.createComment` line 92
    
    * Update preliminary-update-comment.js
    
    edited to `github.rest.issues.xxx` lines 44, 123
    
    * Update issue-trigger.yml
    
    * Update issue-trigger.yml

--- github-actions/trigger-issue/add-missing-labels-to-issues/post-labels-comment.js
+++ github-actions/trigger-issue/add-missing-labels-to-issues/post-labels-comment.js
@@ -89,7 +89,7 @@ function makeComment(labels) {
  */
  async function postComment(issueNum, comment) {
   try {
-    await github.issues.createComment({
+    await github.rest.issues.createComment({
       owner: context.repo.owner,
       repo: context.repo.repo,
       issue_number: issueNum,
