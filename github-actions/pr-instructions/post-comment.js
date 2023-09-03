1cb58ad596659038e11e08ca304b4a803c341d6f
Roslyn Wythe <roslynwythe@gmail.com>
2023-09-03 02:56:13 -0700

    refactor pr instruction logic and add instruction for viewing changes to CONTRIBUTING.md

--- github-actions/pr-instructions/post-comment.js
+++ github-actions/pr-instructions/post-comment.js
@@ -16,16 +16,9 @@ async function main({ g, c }, { issueNum, instruction }) {
     github = g;
     context = c;
 
-    const instructions = formatComment(instruction)
-    postComment(issueNum, instructions);
+    postComment(issueNum, decodeURI(instruction));
 }
 
-function formatComment(instruction) {
-    const path = './github-actions/pr-instructions/pr-instructions-template.md'
-    const text = fs.readFileSync(path).toString('utf-8');
-    const completedInstuctions = text.replace('${commandlineInstructions}', instruction)
-    return completedInstuctions
-}
 
 async function postComment(issueNum, instructions) {
     try {
