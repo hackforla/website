fc6e19f67be755867070c2d48ed49a78b63b69d7
Roslyn Wythe <roslynwythe@gmail.com>
2023-09-03 20:56:36 -0700

    moved decodeURI back into postcomment

--- github-actions/pr-instructions/post-comment.js
+++ github-actions/pr-instructions/post-comment.js
@@ -15,7 +15,7 @@ var context;
 async function main({ g, c }, { issueNum, instruction }) {
     github = g;
     context = c;
-    postComment(issueNum, instruction);
+    postComment(issueNum, decodeURI(instruction));
 }
 
 
