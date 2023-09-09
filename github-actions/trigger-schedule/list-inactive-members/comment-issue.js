62afa80ed934016824d91d24e3f4c712e0304de6
Will Gillis <40799239+t-will-gillis@users.noreply.github.com>
2023-09-08 19:19:35 -0700

    Update comment-issue.js
    Consolidate `schedule-monthly.yml` and `schedule-thu-1100.yml`

--- github-actions/trigger-schedule/list-inactive-members/comment-issue.js
+++ github-actions/trigger-schedule/list-inactive-members/comment-issue.js
@@ -8,10 +8,12 @@ async function main({ g, c }, newIssueNumber) {
   github = g;
   context = c;
 
+  // Issue #2607 is the `Dev/PM Agenda and Notes` 
   let agendaAndNotesIssueNumber = 2607;
   await commentOnIssue(agendaAndNotesIssueNumber, newIssueNumber);
 }
 
+// Add a link to the `Review Inactive Team Members` issue
 const commentOnIssue = async (agendaAndNotesIssueNumber, newIssueNumber) => {
   const owner = "hackforla";
   const repo = "website";
@@ -19,7 +21,7 @@ const commentOnIssue = async (agendaAndNotesIssueNumber, newIssueNumber) => {
     owner,
     repo,
     issue_number: agendaAndNotesIssueNumber,
-    body: `**Review Inactive Members:** #${newIssueNumber}`,
+    body: `**Review Inactive Team Members:** #${newIssueNumber}`,
   });
 };
 
