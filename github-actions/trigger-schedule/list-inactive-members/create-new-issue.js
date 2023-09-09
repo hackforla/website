fa8230e71aae6b45c01e36c1d96fc274d77d1bda
Will Gillis <40799239+t-will-gillis@users.noreply.github.com>
2023-09-08 19:15:43 -0700

    Update create-new-issue.js

--- github-actions/trigger-schedule/list-inactive-members/create-new-issue.js
+++ github-actions/trigger-schedule/list-inactive-members/create-new-issue.js
@@ -1,40 +1,73 @@
 // Import modules
+const fs = require("fs");
 
 // Global variables
 var github;
 var context;
 
-async function main({ g, c }, list) {
+async function main({ g, c }, artifactContent) {
   github = g;
   context = c;
 
+  // Retrieve lists data from json file written in previous step
+  let inactiveLists = JSON.parse(artifactContent);
+  
   const owner = "hackforla";
   const repo = "website";
 
-  // create a new issue in repo, return the issue id for later: creating the project card linked to this issue
-  const issue = await createIssue(owner, repo, list);
+  // Create a new issue in repo, return the issue id for later: creating the project card linked to this issue
+  const issue = await createIssue(owner, repo, inactiveLists);
   const issueId = issue.id;
   const issueNumber = issue.number;
-  // get project id, in order to get the column id of `New Issue Approval` in `Project Board`
+  // Get project id, in order to get the column id of `New Issue Approval` in `Project Board`
   const projectId = await getProjectId(owner, repo);
-  // get column id, in order to create a project card in `Project Board` and place in `New Issue Approval`
+  // Get column id, in order to create a project card in `Project Board` and place in `New Issue Approval`
   const columnId = await getColumnId(projectId);
-  // create the project card, which links to the issue created on line 16
+  // Create the project card, which links to the issue created in createIssue() above
   await createProjectCard(issueId, columnId);
-  // return issue number: going to be using this number to link the issue when commenting on the `Dev/PM Agenda and Notes`
+  // Return issue number used to reference the issue when commenting on the `Dev/PM Agenda and Notes`
   return issueNumber;
 }
 
-const createIssue = async (owner, repo, list) => {
-  let listWithNewLine = list.join("\n");
-  let body = "**Inactive Members:**\n\n" + listWithNewLine;
+const createIssue = async (owner, repo, inactiveLists) => {
+  // Splits inactivesList into lists of removed contributors and of those to be notified
+  let removeList = inactiveLists['removedContributors'];
+  let notifyList = inactiveLists['notifiedContributors'];
+
+  let removedList = removeList.map(x => "@" + x).join("\n");  
+  let notifiedList = notifyList.map(x => "@" + x).join("\n"); 
+  
+  let title = "Review Inactive Team Members";
+  let body = "# Review of Inactive Website Team Members\n" 
+  + "## Inactive Members\n"
+  + "Developers: If your name is on the following list, our team bot has determined that you have not been active with the Website team in the last 30 days. If we don't hear back from you in the upcoming weeks, we will unassign you from any issues you may be working on and remove you from the 'website-write' team.\n\n"
+  + notifiedList + "\n\n"
+  + "## Removed Members\n"
+  + "Our team bot has determined that the following member(s) have not been active with the Website team for over 60 days, and therefore the member(s) have been removed from the 'website-write' team.\n\n"
+  + removedList + "\n\n\n\n"
+  + "### If this is a mistake or if you would like to return to the Hack for LA Website team, please respond in a comment with one of the two following messages:\n"
+  + "#### Our bot made a mistake, let us know!\n"
+  + "```\n"
+  + "I have been active!  See Issue # or PR # \n"
+  + "```\n"
+  + "#### I want to come back to the team!\n"
+  + "```\n"
+  + "Please add me back to the write team, I am ready to work on an issue now.\n"
+  + "```\n"
+  + "### If it has been less than 20 days:\n"
+  + "- Make sure that you are assigned to an issue, and\n"
+  + "- That issue is in the column \"In progress (actively working)\", and\n"
+  + "- You are providing weekly progress updates.\n"
+  + "### If it has been more than 20 days:\n"
+  + "- Please send us a Slack message with a link to your comment\n\n"
   let labels = [
+    "Feature: Administrative",
+    "role: dev leads",
     "Ready for dev lead",
     "Ready for product",
     "Complexity: Small",
     "Size: 0.5pt",
   ];
-  const title = "Review Inactive Members";
   const issue = await github.rest.issues.create({
     owner,
     repo,
@@ -46,12 +79,12 @@ const createIssue = async (owner, repo, list) => {
 };
 
 const getProjectId = async (owner, repo) => {
-  // get all projects for the repo
+  // Get all projects for the repo
   let projects = await github.rest.projects.listForRepo({
     owner,
     repo,
   });
-  // select project with name `Project Board`, access the `id`
+  // Select project with name `Project Board` then access the project `id`
   let projectId = projects.data.filter((project) => {
     return (project.name = "Project Board");
   })[0].id;
@@ -59,11 +92,11 @@ const getProjectId = async (owner, repo) => {
 };
 
 const getColumnId = async (projectId) => {
-  // get all columns in the project board
+  // Get all columns in the project board
   let columns = await github.rest.projects.listColumns({
     project_id: projectId,
   });
-  // select column with name `New Issue Approval`, access the `id
+  // Select column with name `New Issue Approval` then access the column `id`
   let columnId = columns.data.filter((column) => {
     return column.name === "New Issue Approval";
   })[0].id;
