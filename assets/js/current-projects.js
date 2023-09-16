81e18cc39670ca8ac02de9503ae74149926dd086
Billy <billy.phan4@gmail.com>
2023-09-15 23:40:30 -0700

    search function added back

--- assets/js/current-projects.js
+++ assets/js/current-projects.js
@@ -1,5 +1,7 @@
 ---
+
 ---
+
 // Do All Dom Manipulation After The DOM content is full loaded
 document.addEventListener("DOMContentLoaded",function(){
     (function main(){
@@ -15,7 +17,7 @@ document.addEventListener("DOMContentLoaded",function(){
 
         // create filter dictionary from sorted project data
         let filters = createFilter(sortedProjectData);
-
+        
         // Insert Checkbox Filter Into The Dom
         for(let [filterName,filterValue] of Object.entries(filters)){
             // Add displayed filter title, resolves issue of "program areas" not being valid html attribute name due to spacing
@@ -39,15 +41,19 @@ document.addEventListener("DOMContentLoaded",function(){
                 document.getElementById(filterName).insertAdjacentHTML( 'beforeend', `<li class="view-all" tabindex="0" role="button" aria-label="View All ${filterTitle} Filters">View all</li>` );
             }
         }
+
         document.querySelectorAll("input[type='checkbox']").forEach(item =>{
             item.addEventListener('change', checkBoxEventHandler)
         });
+
         document.querySelectorAll("li.view-all").forEach(viewAll => {
             viewAll.addEventListener("click", viewAllEventHandler)
         })
+
         document.querySelectorAll(".labelArrow").forEach(arrow => {
             arrow.addEventListener("click", showNoneEventHandler)
         })
+
         document.querySelectorAll(".show-filters-button").forEach(button => {
             button.addEventListener("click", showFiltersEventHandler)
         })
@@ -56,7 +62,7 @@ document.addEventListener("DOMContentLoaded",function(){
         })
         document.querySelector(".cancel-mobile-filters").addEventListener("click", cancelMobileFiltersEventHandler)
         document.addEventListener('keydown', tabFocusedKeyDownHandler);
-
+        
         //events related to search bar
         document.querySelector("#search").addEventListener("focus",searchOnFocusEventHandler);
         document.querySelector("#search").addEventListener("keydown", searchEnterKeyHandler);
@@ -96,6 +102,7 @@ function retrieveProjectDataFromCollection(){
             projectLanguagesArr.push(projectLanguages);
         }
     })
+
     let projectData = [{%- for project in visible_projects -%}
             {
                 "project": {
@@ -154,6 +161,7 @@ function retrieveProjectDataFromCollection(){
     })
     return projectData;
 }
+
 /**
  * Given an input of a project data array object as returned by the function `retrieveProjectDataFromCollection()`, this
  * function sorts the project twice.
@@ -161,10 +169,13 @@ function retrieveProjectDataFromCollection(){
  *  2. It sort all project by title for each status type
 */
 function projectDataSorter(projectdata){
+
     const statusList = ["Active","Completed","On Hold"]
     const sortedProjectContainer = [];
+
     // Sort Project data by status alphabetically
     projectdata.sort( (a,b) => (a.project.status > b.project.status) ? 1 : -1)
+
     // Sort Project Data by title for each status type
     for(const status of statusList){
             let arr = projectdata.filter(function(item){
@@ -172,8 +183,10 @@ function projectDataSorter(projectdata){
         }).sort( (a,b) => (a.project.title > b.project.title) ? 1 : -1);
         sortedProjectContainer.push(...arr);
     }
+
     return sortedProjectContainer;
 }
+
 /**
  * Given an array of project object as returned by ``retrieveProjectDataFromCollection()``
  * Returns a filter object -> {filter_type1:[filter_value1,filter_value2], filter_type2:[filter_value1,filter_value2], ... }
@@ -185,8 +198,10 @@ function createFilter(sortedProjectData){
             'programs': [...new Set(sortedProjectData.map(item => item.project.programAreas ? item.project.programAreas.map(programArea => programArea) : '').flat() ) ].filter(v=>v!='').sort(),
             'technologies': [...new Set(sortedProjectData.map(item => (item.project.technologies && item.project.languages?.length > 0) ? [item.project.languages, item.project.technologies].flat() : '').flat() ) ].filter(v=>v!='').sort(),
             'status': [... new Set(sortedProjectData.map(item => item.project.status))].sort(),
+
         }
 }
+
 /**
  * Update the history state and the url parameters on checkbox changes
 */
@@ -223,7 +238,7 @@ function checkBoxEventHandler(){
         window.history.replaceState(null,'',`?${queryString}`)
     }
 
-
+    
 }
 //shows all filters for a category
 function viewAllEventHandler(e) {
@@ -259,7 +274,7 @@ function searchEventHandler(e){
     let searchTerm=document.querySelector("#search").value;
     let tokenObj={};
     tokenObj['Search']=searchTerm;
-
+     
    const filterParams = Object.fromEntries(new URLSearchParams(window.location.search));
     if (Object.keys(filterParams).length>0) {    
         for(const [key,value] of Object.entries(filterParams)){
@@ -303,19 +318,19 @@ function updateUI(){
 
     //If there are no entries in URL display clear all filter tags and display all cards
     if(Object.keys(filterParams).length === 0 ){noUrlParameterUpdate(filterParams) };
-    
+
     // Filters listed in the url parameter are checked or unchecked based on filter params
     updateCheckBoxState(filterParams);
-    
+
     // Update category counter based on filter params
     updateCategoryCounter(filterParams)
-    
+
     // Card is shown/hidden based on filters listed in the url parameter
     updateProjectCardDisplayState(filterParams);
 
     //update this later!!!
     toggleNoResultMsgIfNoMatch(filterParams, 'project-card')
-    
+
     // The function updates the frequency of each filter based on the cards that are displayed on the page.
     updateFilterFrequency(filterParams);
 
@@ -324,14 +339,14 @@ function updateUI(){
 
     // Add onclick event handlers to filter tag buttons and a clear all button if filter-tag-button exists in the dom
     attachEventListenerToFilterTags()
-
+    
 }
 
     /**
      * Computes and return the frequency of each checkbox filter that are currently present in on the displayed cards on the page
  */
 function updateFilterFrequency(){
-
+    
         const onPageFilters = []
         // Push the filters present on the displayed cards on the page into an array.
         document.querySelectorAll('.project-card[style*="display: list-item;"]').forEach(card => {
@@ -362,7 +377,7 @@ function updateFilterFrequency(){
         for(const [key,value] of Object.entries(filterFrequencyObject)){
             document.querySelector(`label[for="${key.split("_")[1]}"]`).lastElementChild.innerHTML = ` (${value})`;
         }
-
+    
 }
 
     /**
@@ -393,16 +408,16 @@ function updateCategoryCounter(filterParams){
         for(const [key,value] of container){
             document.querySelector(`#${key}`).innerHTML = ` (${value})`;
         }
-
+    
 }
     /**
      * Card is shown/hidden based on filters listed in the url parameter
  */
 function updateProjectCardDisplayState(filterParams){
-
+    
     document.querySelectorAll('.project-card').forEach(projectCard => {
         const projectCardObj = {};
-
+               
         for(const key in filterParams){
             if(key !=='Search'){            
                 projectCardObj[key] = projectCard.dataset[key].split(",");
@@ -412,10 +427,10 @@ function updateProjectCardDisplayState(filterParams){
                 for(const area of searchAreas){
                     projectCardObj[area]=projectCard.dataset[area].split(",");
                 }
-
+                
             }
         }
-
+        
         const cardsToHideContainer = [];
         for(const [key,value] of Object.entries(filterParams)){
             if(key !=='Search'){
@@ -459,7 +474,7 @@ function updateProjectCardDisplayState(filterParams){
                                 }
                             }
                         }
-
+                        
                         i++;                        
                     } 
                     else if(token.includes("-")){
@@ -468,9 +483,9 @@ function updateProjectCardDisplayState(filterParams){
                         let noMatchDataset=0;
                         for(const [key,value] of Object.entries(projectCardObj)){
                             if(  value.filter(x => searchRegex.test(x) ).length > 0){
-
+                                
                                 cardsToHideContainer.push([key,projectCard.id]);
-
+                            
                             }
                         }
                     }
@@ -502,33 +517,42 @@ function updateFilterTagDisplayState(filterParams){
     // Clear all filter tags
     document.querySelectorAll('.filter-tag').forEach(filterTag => filterTag.parentNode.removeChild(filterTag) );
     document.querySelectorAll('.applied-filters').forEach(appliedFilters => appliedFilters.parentNode.removeChild(appliedFilters) );
+
     //Filter tags display hide logic
     for(const [key,value] of Object.entries(filterParams)){
         value.forEach(item =>{
             document.querySelector('.filter-tag-container').insertAdjacentHTML('afterbegin', filterTagComponent(key,item ) );
+
         })
+
     }
+
     if (Object.entries(filterParams). length > 0) {
         document.querySelector('.filter-tag-container').insertAdjacentHTML('afterbegin', `<h4 class="applied-filters">Applied Filters</h4>`)
     }
 }
+
     /**
      * Add onclick event handlers to filter tag buttons and a clear all button if filter-tag-button exists in the dom
  */
 function attachEventListenerToFilterTags(){
     if(document.querySelectorAll('.filter-tag').length > 0){
+
         // Attach event handlers to button
         document.querySelectorAll('.filter-tag').forEach(button => {
             button.addEventListener('click',filterTagOnClickEventHandler)
         })
+
         // If there exist a filter-tag button on the page add a clear all button after the last filter tag button
         if(!document.querySelector('.clear-filter-tags')){
             document.querySelector('.filter-tag:last-of-type').insertAdjacentHTML('afterend',`<a class="clear-filter-tags" tabindex="0" aria-label="Clear All Filters" style="white-space: nowrap;">Clear All</a>`);
+
             //Attach an event handler to the clear all button
             document.querySelector('.clear-filter-tags').addEventListener('click',clearAllEventHandler);
         }
     }
 }
+
 /**
  * If there are no url parameter
  *  1. Display all cards
@@ -539,37 +563,51 @@ function attachEventListenerToFilterTags(){
 function noUrlParameterUpdate(){
     // Display all cards
     document.querySelectorAll('.project-card').forEach(projectCard => {  projectCard.style.display = 'list-item'  });
+
     // Clear all checkboxes
     document.querySelectorAll("input[type='checkbox']").forEach(checkBox => {checkBox.checked = false});
+
     // Clear all number of checkbox counters
     document.querySelectorAll('.number-of-checked-boxes').forEach(checkBoxCounter => {checkBoxCounter.innerHTML = ''} );
+
     // Clear all filter tags
     document.querySelectorAll('.filter-tag') && document.querySelectorAll('.filter-tag').forEach(filterTag => filterTag.remove() );
+
     // Remove Clear All Button
     document.querySelector('.clear-filter-tags') && document.querySelector('.clear-filter-tags').remove();
     return;
 }
+
 /**
  * Filter Tag Button On Click Event Handler
  *  The function removes key:value from url parameter based on the filter-tag button clicked on
 */
 function filterTagOnClickEventHandler(){
+
     //Get filter parameters from the url
     const filterParams = Object.fromEntries(new URLSearchParams(window.location.search));
+
     //Transform filterparam object values to arrays
     Object.entries(filterParams).forEach( ([key,value]) => filterParams[key] = value.split(',') )
+
+
+
     let buttonClickedData = Object.fromEntries([ this.dataset.filter.split(",") ])
+
     for(const [button_filtername,button_filtervalue] of Object.entries(buttonClickedData)){
         if(filterParams[button_filtername].includes(button_filtervalue) ){
             filterParams[button_filtername].splice( filterParams[button_filtername].indexOf(button_filtervalue), 1);
             filterParams[button_filtername].length == 0 && delete filterParams[button_filtername];
         }
     }
+
     // Prepare Query String
     let queryString = Object.keys(filterParams).map(key => key + '=' + filterParams[key]).join('&').replaceAll(" ","+");
+
     //Update URL parameters
     window.history.replaceState(null, '', `?${queryString}`);
 }
+
 /**
  * Clear All Button Event Handler
  *  The function clears all URL parameter by setting the history to '/'
@@ -579,6 +617,7 @@ function clearAllEventHandler(){
     //Update URL parameters
     window.history.replaceState(null, '', '/');
 }
+
 /**
  * Takes a single project object and returns the html string representing the project card
 */
@@ -595,21 +634,27 @@ function projectCardComponent(project){
                 data-title="${project.title}"
             >
                 <div class="project-card-inner">
+
                     <a href='${project.id}'>
                         <div class="project-tmb">
                             <img src='${window.location.origin}${project.image}' class="project-tmb-img" alt='${project.alt}'/>
                         </div>
                     </a>
+
                     <div class="project-body">
                         <div class='status-indicator status-${project.status}'>
                         <h5 class='status-text'>${ project.status }</h5>
                         </div>
+
                         <a href='${ project.id }'><h4 class="project-title">${ project.title }</h4></a>
+
                         <p class="project-description">${ project.description }</p>
+
                         <div class="project-links">
                         <strong>Links: </strong>
                         ${project.links.map(item => `<a href="${ item.url }" rel="noopener" target='_blank'> ${ item.name }</a>`).join(", ")}
                         </div>
+
                         ${project.partner ?
                         `
                         <div class="project-partner">
@@ -618,6 +663,7 @@ function projectCardComponent(project){
                         </div>
                         `:""
                         }
+
                         ${project.tools ?
                         `
                         <div class="project-tools">
@@ -626,6 +672,7 @@ function projectCardComponent(project){
                         </div>
                         `: ""
                         }
+
                         ${project.looking ? "" : ""
                         // `
                         // <div class="project-needs">
@@ -635,6 +682,7 @@ function projectCardComponent(project){
                         // `:""
                         // ^ See issue #1997 for more info on why this is commented out
                         }
+
                         ${project.languages?.length > 0 ? 
                         `
                         <div class="project-languages">
@@ -643,6 +691,7 @@ function projectCardComponent(project){
                         </div>
                         `: ""
                         }
+
                         ${project.technologies ?
                         `
                         <div class="project-technologies">
@@ -651,6 +700,7 @@ function projectCardComponent(project){
                         </div>
                         `:""
                         }
+
                         ${project.programAreas ?
                         `
                         <div class="project-programs">
@@ -688,10 +738,12 @@ function dropDownFilterComponent(categoryName,filterArray,filterTitle){
     </li>
     `
 }
+
 /**
  * Takes a name of a checkbox filter and the value of the check boxed filter
  * and creates a html string representing a button
 */
+
 function filterTagComponent(filterName,filterValue){
     return `<div
                 data-filter='${filterName},${filterValue}'
@@ -701,4 +753,12 @@ function filterTagComponent(filterName,filterValue){
                 ${filterName === "looking" ? "Role" : filterName}: ${filterValue}
                 </span>
             </div>`
+}
+
+function toggleNoResultMsgIfNoMatch(filtersParams,querySelector) {
+    if ([...document.querySelectorAll(`.${querySelector}`)].every(card => card.style.display === 'none')) {
+        noResultsMessageComponent(filtersParams,'black')
+    } else {
+        document.querySelector(".no-results-message").innerHTML = ""
+    }
 }
