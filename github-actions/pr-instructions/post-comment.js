7366956b32c0feefe88075db7d718beeafe6255d
Roslyn Wythe <roslynwythe@gmail.com>
2023-11-02 23:07:37 -0700

    Please review but don't merge yet: Update PR Instruction GHA to add instruction for previewing CONTRIBUTING.md (#5438)
    * refactor pr instruction logic and add instruction for viewing changes to
    CONTRIBUTING.md
    
    * created a variable to hold the decoded instruction
    
    * moved decodeURL out of postComment into wr-pr-instructions.yml
    
    * moved decodeURI back into postcomment
    
    * moved comment formatting to create-instruction.js, added new instruction
    for viewing changes to CONTRIBUTING.md, added URI encoding to avoid
    problems with backtick character
    Please enter the commit message for your changes. Lines starting
    
    * new template for instructions for previewing CONTRIBUTING.md

--- github-actions/pr-instructions/post-comment.js
+++ github-actions/pr-instructions/post-comment.js
@@ -6,7 +6,8 @@ var github;
 var context;
 
 /**
- * Formats the commandline instructions into a template, then posts it to the pull request.
+ * Use decodeURI() to decode the instruction prior to posting to PR
+ * (URI Encoding was applied in create-instruction.js to handle the backtick character)
  * @param {Object} g - github object  
  * @param {Object} c - context object 
  * @param {Number} issueNum - the number of the issue where the post will be made 
@@ -15,17 +16,9 @@ var context;
 async function main({ g, c }, { issueNum, instruction }) {
     github = g;
     context = c;
-
-    const instructions = formatComment(instruction)
-    postComment(issueNum, instructions);
+    postComment(issueNum, decodeURI(instruction));
 }
 
-function formatComment(instruction) {
-    const path = './github-actions/pr-instructions/pr-instructions-template.md'
-    const text = fs.readFileSync(path).toString('utf-8');
-    const completedInstuctions = text.replace('${commandlineInstructions}', instruction)
-    return completedInstuctions
-}
 
 async function postComment(issueNum, instructions) {
     try {
