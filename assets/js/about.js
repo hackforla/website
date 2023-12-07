91ed3082128e0ffbab2fca54bf1a925acf8ddbcb
Thinking-Panda <122488603+Thinking-Panda@users.noreply.github.com>
2023-12-07 12:02:06 -0800

    Accomplishments edit by year 4188 (#5932)
    * accomplishments-year-check
    
    * Merge branch 'gh-pages' into accomplishments-about-page-different-years-4188
    
    * edits
    
    * syntax-clean
    
    * linter-clean-up
    
    * syntax-clean-up
    
    * changes-after-review
    
    * Sample-accomplishmetns-clean-up

--- assets/js/about.js
+++ assets/js/about.js
@@ -1,3 +1,7 @@
+---
+
+---
+
 // Constant setups for reuse throughout document
 const ANCHOR_LINKS_ID = [...document.querySelectorAll(".anchor")].map((anchor) => anchor.id);
 const NAVIGATION_LINK_ELEMENTS = document.querySelectorAll(".sticky-nav a");
@@ -11,8 +15,14 @@ const FLAGS = {
 
 document.addEventListener("DOMContentLoaded", function () {
   
+  const accomplishments=retrieveAccomplishmentsData();
+  const accByProgramArea=sortAccomplishmentsByProgramArea(accomplishments);
+  displayAccomplishments(accByProgramArea);
+
   // UI components are updated on page load
   updateUI();
+
+  loadAccomplishmentYears();
   
   // UI components are updated on location change
   window.addEventListener("locationchange", updateUI);
@@ -22,6 +32,9 @@ document.addEventListener("DOMContentLoaded", function () {
   document.addEventListener("wheel", scrollHandler);
 
   document.querySelectorAll("#sticky-nav li a").forEach((navlink) => { navlink.addEventListener("click", toggleNavClass); });
+  
+  document.querySelector("#accomplishments-year-dropdown").addEventListener("change", function(event) {yearEventHandler(accByProgramArea,event)});
+  document.querySelectorAll("#accomplishments-year-list li").forEach((item)=>{item.addEventListener("click", function(event) {yearEventHandler(accByProgramArea,event)})});
 });
 
 /**
@@ -50,18 +63,18 @@ function updateUI() {
   }
   setupAccordionEventLitseners();
   changeDisplayMode();
-  stickItHere();
+  stickItHere();  
 }
 
 function changeDisplayMode() {
   if (window.innerWidth < BREAK_POINT && FLAGS.displayChanged) {
-    document.querySelectorAll(".page-card--about:nth-child(n+3)").forEach((pageCard) => {pageCard.lastElementChild.style.display = "none"; });
+    document.querySelectorAll(".page-card--about:nth-child(n+3)").forEach((pageCard) => {pageCard.lastElementChild.style.display = "none"; });   
     document.querySelector(`div[data-hash="${window.location.hash.split("#")[1]}"]`).parentElement.lastElementChild.style.display = "block";
     document.querySelector(`div[data-hash="${window.location.hash.split("#")[1]}"]`).parentElement.children[0].classList.toggle("au_active")
     FLAGS.displayChanged = false;
   } 
   else if (window.innerWidth > BREAK_POINT && !FLAGS.displayChanged) {
-    document.querySelectorAll(".page-card--about:nth-child(n+3)").forEach((pageCard) => { pageCard.lastElementChild.style.display = "block"; });
+    document.querySelectorAll(".page-card--about:nth-child(n+3)").forEach((pageCard) => { pageCard.lastElementChild.style.display = "block"; });   
     document.querySelectorAll(".au_active").forEach((pageCard) => { pageCard.classList.remove("au_active"); });
     FLAGS.displayChanged = true;
   }
@@ -126,6 +139,20 @@ function stickItHere() {
   }
 }
 
+//This function lists the years in the Accomplishments 
+function loadAccomplishmentYears() {
+  let d = new Date();
+  let year = d.getFullYear();
+  for (let i=year; i>=2020; i--){
+    let optionsHtml='<option value="' + i + '">' + i + '</option>';
+    document.querySelector("#accomplishments-year-dropdown").insertAdjacentHTML ('beforeend',optionsHtml);
+  }
+  for (let i=year; i>=2020; i--){
+    let listItemHtml='<li id="' + i + '">' + i + '</li>';
+    document.querySelector("#accomplishments-year-list").insertAdjacentHTML ('beforeend',listItemHtml);
+  }  
+}
+
 
 /**
  * Alignment of page card with its corresponding link on the sticky navigation
@@ -184,4 +211,103 @@ window.addEventListener(
 
   
   }
-)
+);
+
+function retrieveAccomplishmentsData() {
+  const scriptTag = document.getElementById("aboutScript");
+  let receivedData = scriptTag.getAttribute("accomplishments");
+  let accData=receivedData.replaceAll("}{", "},{").replaceAll("=>",":");
+  let jsonFormat="[" + accData + "]";
+  const accomplishmentsData=JSON.parse(jsonFormat);
+  return accomplishmentsData;
+}
+
+function sortAccomplishmentsByProgramArea(accomplishments){
+  let accData=[...accomplishments];
+  {% assign projects = site.projects %}
+  let projectData=[{%- for project in projects -%}
+                      {"project-path":"{{project.path | split: '/' | last}}","program-area": "{{project.program-area}}"}
+                      {%- unless forloop.last -%}, {% endunless %}
+                  {%- endfor -%}];
+  accData.forEach(acc => {
+    if (acc["project-file"]){
+      projectData.forEach(project => {
+        if (project["project-path"] == acc["project-file"]){
+          acc["program-area"] = project["program-area"];
+        }
+      })
+    }
+  });
+  accData.sort( (a,b) => (a["program-area"] > b["program-area"]) ? 1 : -1);
+  return (accData);  
+}
+
+function yearEventHandler(accByProgramArea,e){
+  let items=document.querySelectorAll("#accomplishments-year-list li");
+  items.forEach(year=>year.style.color="black");
+  let element=e.target.nodeName;
+  let accYear="";
+  let accToDisplay=[];
+  if(element=="LI"){ 
+    e.target.style.color = "rgb(250, 17, 79)";
+    accYear=e.target.id;
+    document.querySelector("#accomplishments-year-dropdown").value=accYear;
+  }
+  else {
+    accYear=e.target.value;
+    let element=document.getElementById(accYear);
+    element.style.color="rgb(250,17,80,255)";
+  }
+  
+  if (accYear == "All"){
+    accToDisplay=accByProgramArea;
+  }
+  else {
+    let year=parseInt(accYear,10);
+    accByProgramArea.forEach(acc => {
+      if(acc.year== year) {
+        accToDisplay.push(acc);
+      }
+    })
+  }
+  displayAccomplishments(accToDisplay);
+}
+
+function displayAccomplishments(accToDisplay) {  
+  document.querySelector(".top-left-column--acc").innerHTML="";
+  document.querySelector(".bottom-right-column--acc").innerHTML="";
+  if(accToDisplay.length ==0){
+    document.querySelector(".top-left-column--acc").innerHTML="<p class='wip-msg'>Work In Progress...</p>";
+  }
+  let progAreaList=[];
+  let accCount = accToDisplay.length;
+  let colItems = Math.ceil(accCount/2);
+  let counter = 1;
+  accToDisplay.forEach(acc => {
+    if(!progAreaList.includes(acc["program-area"])) {
+      progAreaList.push(acc["program-area"]);
+    }      
+  })
+  for( let i=0; i<=progAreaList.length-1;i++){
+    let progAreaHtml='<div class="accomplishment-program-area">' + progAreaList[i] + '</div>';
+    if (counter <= colItems){
+      document.querySelector(".top-left-column--acc").insertAdjacentHTML("beforeend",progAreaHtml);
+    }
+    else {
+      document.querySelector(".bottom-right-column--acc").insertAdjacentHTML("beforeend",progAreaHtml);
+    }
+    accToDisplay.forEach(acc => { 
+      if(acc["program-area"]== progAreaList[i] && counter<=colItems){
+        counter++;
+        let accHtml= ` <div class="accomplishment-title"><a href="${acc.link}" target="_blank" rel="noopener noreferrer"> ${acc.title} </a></div>
+                      <div> ${acc.description} </div>`;
+        document.querySelector(".top-left-column--acc").insertAdjacentHTML("beforeend",accHtml);
+      }
+      else if (acc["program-area"]== progAreaList[i] && counter>colItems) {
+        let accHtml= ` <div class="accomplishment-title"><a href="${acc.link}" target="_blank" rel="noopener noreferrer"> ${acc.title} </a></div>
+                      <div> ${acc.description} </div>`;
+        document.querySelector(".bottom-right-column--acc").insertAdjacentHTML("beforeend",accHtml);
+      }
+    }) 
+  }
+}
