d9272925080658dbde0cd9e1cb65199964165dac
David B <d.jbradley.ii@gmail.com>
2024-01-23 01:01:39 -0800

    Adds missing size to requirements'

--- github-actions/trigger-issue/add-missing-labels-to-issues/check-labels.js
+++ github-actions/trigger-issue/add-missing-labels-to-issues/check-labels.js
@@ -1,5 +1,5 @@
 // Constant variables
-const REQUIRED_LABELS = ['Complexity', 'role', 'Feature']
+const REQUIRED_LABELS = ['Complexity', 'role', 'Feature', 'size']
 const LABEL_MISSING = ['Complexity: Missing', 'role missing', 'Feature Missing', 'size: missing']
 const COMPLEXITY_EXCEPTIONS = ['good first issue']
 
