622372c7c2fdc77feb08954a7e72e208c908a181
Ian Cooperman <34320199+iancooperman@users.noreply.github.com>
2024-03-04 20:16:41 -0800

    Setting columnId myself instead of using a secret for testing purposes

--- github-actions/trigger-schedule/add-update-label-weekly/add-label.js
+++ github-actions/trigger-schedule/add-update-label-weekly/add-label.js
@@ -31,6 +31,7 @@ async function main({ g, c }, columnId) {
   console.log("Hello GitHub Action World!");
   github = g;
   context = c;
+  columnId = 6;
   // Retrieve all issue numbers from a column
   const issueNums = getIssueNumsFromColumn(columnId);
   console.log(columnId);
