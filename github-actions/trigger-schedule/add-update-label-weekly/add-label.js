a2c9f6949d7f7bd78cf5e79229afe11829a6126b
Ian Cooperman <34320199+iancooperman@users.noreply.github.com>
2024-03-05 18:21:11 -0800

    this time?

--- github-actions/trigger-schedule/add-update-label-weekly/add-label.js
+++ github-actions/trigger-schedule/add-update-label-weekly/add-label.js
@@ -135,7 +135,7 @@ function isTimelineOutdated(timeline, issueNum, assignees) { // assignees is an 
 
     let eventTimestamp = eventObj.updated_at || eventObj.created_at;
     eventTimestamp = new Date().setDate(new Date(eventTimestamp).getDate() - 8);
-    eventTimestamp = eventTimestamp.toDateString(); // pretend the eventTimestamp is 8 days ago for testing purposes
+    eventTimestamp = eventTimestamp.toString(); // pretend the eventTimestamp is 8 days ago for testing purposes
     console.log(eventTimestamp);
 
     // update the lastCommentTimestamp if this is the last (most recent) comment by an assignee
