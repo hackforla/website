9ead45d4b38f051fa36a34abe815d077b5c5d61c
Raviteja <gteja1998@gmail.com>
2024-03-06 22:09:36 -0500

    Updated requested changes variable name and indentation of file

--- assets/js/elements/noResultsMessageFilter.js
+++ assets/js/elements/noResultsMessageFilter.js
@@ -1,15 +1,13 @@
-
-
-    function noResultsMessageComponent(filterParams, textColor) {
-        let filterList = ``
-        let hashmap = {"technologies" : "Technology", "languages" : "Language", "tools": "Tool", "programs" : "Program"}
-        for (let key in filterParams) {
-            if (filterParams[key].length) {
-                for (let i = 0; i < filterParams[key].length; i++) {
-                    filterList += `<p>"<span class='toolkit-red'>${hashmap[key]}: ${filterParams[key][i]}</span>"</p>`
-                }
+function noResultsMessageComponent(filterParams, textColor) {
+    let filterList = ``
+    let filterNameSingular = {"technologies" : "Technology", "languages" : "Language", "tools": "Tool", "programs" : "Program"}
+    for (let key in filterParams) {
+        if (filterParams[key].length) {
+            for (let i = 0; i < filterParams[key].length; i++) {
+                filterList += `<p>"<span class='toolkit-red'>${filterNameSingular[key]}: ${filterParams[key][i]}</span>"</p>`
             }
         }
+    }
 
 	const noResultsMsgEle = document.querySelector(".no-results-message")
 	noResultsMsgEle.style.color = textColor
