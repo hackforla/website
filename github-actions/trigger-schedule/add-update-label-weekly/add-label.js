731a915395c9ede9e61d4499e8fa5027d84f0e3d
Ian Cooperman <34320199+iancooperman@users.noreply.github.com>
2024-03-29 18:54:13 -0700

    Removed debug log, no longer needed.

--- github-actions/trigger-schedule/add-update-label-weekly/add-label.js
+++ github-actions/trigger-schedule/add-update-label-weekly/add-label.js
@@ -148,7 +148,6 @@ function isTimelineOutdated(timeline, issueNum, assignees) { // assignees is an 
       lastAssignedTimestamp = eventTimestamp;
     }
 
-    console.log(!isMomentRecent(eventObj.created_at, sevenDayCutoffTime) && isCommentByBot(eventObj) && eventType === 'commented');
     if (!isMomentRecent(eventObj.created_at, sevenDayCutoffTime) && eventType === 'commented' && isCommentByBot(eventObj)) { // If this event did not happen more recently than 7 days ago AND this event is a comment AND the comment is by GitHub Actions Bot, then hide the comment as outdated.
       console.log("Comment created more than 7 days ago. Hiding as outdated...");
       const mutation = JSON.stringify({
