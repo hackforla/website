e548c576e48a830d04bd593114b9f1e66c160dda
Gayle Martin <hello.gmartin@gmail.com>
2024-03-30 09:26:55 -0500

    Update fetch-alerts.js file

--- github-actions/trigger-issue/create-codeql-issues/fetch-alerts.js
+++ github-actions/trigger-issue/create-codeql-issues/fetch-alerts.js
@@ -1,5 +1,4 @@
 const fs = require('fs');
-const core = require('@actions/core');
 
 // Global variables
 var github;
@@ -9,28 +8,31 @@ var context;
  * Fetches a list of open CodeQL alerts from the GitHub API.
  * @returns {Promise<void>} A promise that resolves when the alerts are fetched.
  */
-const fetchAlerts = async ({ g, c, token }) => {
+const fetchAlerts = async ({ g, c }) => {
   // Rename parameters
   github = g;
   context = c; 
 
   // Get a list of open CodeQL alerts
-  const response = await fetch(`https://api.github.com/repos/${context.repo.owner}/${context.repo.repo}/code-scanning/alerts?state=active`, {
-    headers: {
-      Authorization: `token ${token}`
-    },
+  const fetchAlertsResponse = await github.rest.codeScanning.listAlertsForRepo({
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    state: 'open',
+    per_page: 100,
+    page: 1,
   });
 
+
   // Throw error if fetch fails
-  if (!response.ok) {
+  if (!fetchAlertsResponse.ok) {
     throw new Error(`Failed to fetch alerts: ${response.status} - ${response.statusText}`);
   }
 
   // Convert response to JSON
   const alerts = await response.json();
+  console.log('alerts: ', alerts);
 
-  // Set output for future scripts in workflow
-  core.setOutput("alerts", alerts);
+  return alerts;
 };
 
 module.exports = fetchAlerts
