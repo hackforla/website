4346e4272e4cf2ee9b601e6f28020ae1403491f7
Aidan Simmons <aidanwsimmons621@gmail.com>
2024-04-15 12:59:10 -0700

    updated sortData function to resolve unused variable alert

--- assets/js/utility/api-events.js
+++ assets/js/utility/api-events.js
@@ -86,13 +86,14 @@ function filterDataFromApi(responseData) {
  * Sorts Filtered Date from the api end point by their start time
  */
 function sortData(filteredData) {
-  for (const [key, value] of Object.entries(filteredData)) {
-    value.sort(function (a, b) {
+  Object.values(filteredData).forEach(value => {
+    value.sort((a, b) => {
       return convertTime12to24(a.start) - convertTime12to24(b.start) || a.name.localeCompare(b.name);
     });
-  }
+  });
   return filteredData;
 }
+
 
 /**
  * @param {Date} time - A valid javscript time string. Example:  "2020-05-13T02:00:00.000Z"
