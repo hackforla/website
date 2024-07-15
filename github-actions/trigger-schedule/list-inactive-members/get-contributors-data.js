49289219cd3836878fe145e1578a6d91364690b2
Mohamed Sharif <135287156+mSharifHub@users.noreply.github.com>
2024-07-14 19:12:41 -0700

    Update  workflow to account for July, Dec. breaks

--- github-actions/trigger-schedule/list-inactive-members/get-contributors-data.js
+++ github-actions/trigger-schedule/list-inactive-members/get-contributors-data.js
@@ -10,11 +10,11 @@ const maintTeam = 'website-maintain';
 const botMembers = ['elizabethhonest', 'hfla-website-checklist', 'HackforLABot'];
 
 // Set date limits: we are sorting inactive members into groups to warn after 1 month and remove after 2 months.
-// Since the website team takes off the month of December, the January 1st run is skipped (via `schedule-monthly.yml`). 
+// Since the website team takes off the month of December, the January 1st run is skipped (via `schedule-monthly.yml`).
 // The February 1st run keeps the 1 month inactive warning, but changes removal to 3 months inactive (skipping December).
 let today = new Date();
-let oneMonth = (today.getMonth() == 1) ? 2 : 1;            // If month is "February" == 1, then oneMonth = 2 months ago
-let twoMonths = (today.getMonth() == 1) ? 3 : 2;           // If month is "February" == 1, then twoMonths = 3 months ago
+let oneMonth = (today.getMonth() === 1 || today.getMonth() === 8) ? 2 : 1
+let twoMonths = (today.getMonth() === 1 || today.getMonth() === 8) ? 3 : 2
 
 let oneMonthAgo = new Date();                              // oneMonthAgo instantiated with date of "today"
 oneMonthAgo.setMonth(oneMonthAgo.getMonth() - oneMonth);   // then set oneMonthAgo from "today"
@@ -46,15 +46,15 @@ async function main({ g, c }) {
     previousContributors: contributorsTwoMonthsAgo,
     inactiveWithOpenIssue: inactiveWithOpenIssue,
     dates: dates,
-  }; 
+  };
 };
 
 
 
 /**
  * Function to fetch list of contributors with comments/commits/issues since date
- * @returns {Object} allContributorsSinceOneMonthAgo  - List of active contributors since oneMonthAgo 
- * @returns {Object} allContributorsSinceTwoMonthsAgo - List of active contributors since twoMonthsAgo 
+ * @returns {Object} allContributorsSinceOneMonthAgo  - List of active contributors since oneMonthAgo
+ * @returns {Object} allContributorsSinceTwoMonthsAgo - List of active contributors since twoMonthsAgo
  */
 async function fetchContributors(dates){
   let allContributorsSinceOneMonthAgo = {};
@@ -91,26 +91,26 @@ async function fetchContributors(dates){
         // If the API call returns an empty array, break out of loop- there is no additional data.
         // Else if data is returned, push it to `result` and increase the page number (`pageNum`)
         if(!contributors.data.length){
-          break;      
+          break;
         } else {
           result = result.concat(contributors.data);
           pageNum++;
         }
       }
 
-      // Once we have looked at all pages and collected all the data, we create key-value pairs of recent contributors and store 
-      // them in the `allContributorsSince` object. The contributor data that comes back from each API are stored differently,  
+      // Once we have looked at all pages and collected all the data, we create key-value pairs of recent contributors and store
+      // them in the `allContributorsSince` object. The contributor data that comes back from each API are stored differently,
       // i.e. `author.login` vs `user.login` vs `assignee.login`. We want to extract the contributors' usernames for each situation.
       for(const contributorInfo of result){
         // Check if username is stored in `author.login`
         if(contributorInfo.author){
           allContributorsSince[contributorInfo.author.login] = true;
         }
-        // Check for username in `user.login`, but skip `user.login` covered by 3rd API 
+        // Check for username in `user.login`, but skip `user.login` covered by 3rd API
         else if(contributorInfo.user  && api != 'GET /repos/{owner}/{repo}/issues'){
           allContributorsSince[contributorInfo.user.login] = true;
         }
-        // This check is done for `/issues` (3rd) API. Sometimes a user who created an issue is not the same as the 
+        // This check is done for `/issues` (3rd) API. Sometimes a user who created an issue is not the same as the
         // assignee on that issue- we want to make sure that we count all assignees as active contributors as well.
         // We only want to run this check if the assignee is not counted as an active contributor yet.
         else if((contributorInfo.assignee) && (contributorInfo.assignee.login in allContributorsSince === false)){
@@ -121,8 +121,8 @@ async function fetchContributors(dates){
           // If timeline is not outdated, add member to `allContributorsSince`
           if(responseObject.result === false){
             allContributorsSince[assignee] = true;
-          } 
-          // If timeline is more than two months ago, add to open issues with inactive 
+          }
+          // If timeline is more than two months ago, add to open issues with inactive
           // comments with flag = true if issue is "Pre-work Checklist", false otherwise
           else if(date === dates[1]){
             if(contributorInfo.title.includes("Pre-work Checklist")){
@@ -142,14 +142,14 @@ async function fetchContributors(dates){
     for(let i = 0; i < botMembers.length; i++){
       allContributorsSince[botMembers[i]] = true;
     }
-    
+
     if(date === dates[0]){
       allContributorsSinceOneMonthAgo = allContributorsSince;
     } else {
       allContributorsSinceTwoMonthsAgo = allContributorsSince;
     }
-  }   
-  return [allContributorsSinceOneMonthAgo, allContributorsSinceTwoMonthsAgo, inactiveWithOpenIssue]; 
+  }
+  return [allContributorsSinceOneMonthAgo, allContributorsSinceTwoMonthsAgo, inactiveWithOpenIssue];
 }
 
 
@@ -176,7 +176,7 @@ function isEventOutdated(date, timeline, assignee) {
   // If the assignee was assigned later than the 'date', the issue is not outdated so return false
   if (lastAssignedTimestamp && (lastAssignedTimestamp >= date)) {
     return { result: false };
-  } 
+  }
   return { result: true };
 }
 
