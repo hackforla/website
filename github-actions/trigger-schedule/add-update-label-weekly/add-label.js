ecd093e46b0e9b881f59b2ab2578c8024afb8e24
santisecco <santisecco@gmail.com>
2024-09-01 13:22:42 -0300

    Changes to help solve CodeQL altert logging errors

--- github-actions/trigger-schedule/add-update-label-weekly/add-label.js
+++ github-actions/trigger-schedule/add-update-label-weekly/add-label.js
@@ -415,7 +415,7 @@ async function minimizeComment(node_id) {
   });
 
   req.on('error', (error) => {
-    console.error(error);
+    console.error("An error minimizing comments occurred");
   });
 
   req.write(mutation);
