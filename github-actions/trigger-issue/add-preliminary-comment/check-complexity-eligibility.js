6c0a31e92ac3dc9a7e5bd4ac543e17b79ff43057
J <jpham26@gmail.com>
2024-09-06 10:52:22 -0700

    Add formatComment function

--- github-actions/trigger-issue/add-preliminary-comment/check-complexity-eligibility.js
+++ github-actions/trigger-issue/add-preliminary-comment/check-complexity-eligibility.js
@@ -423,4 +423,23 @@ function formatComplexityReminderComment(issueNum, assigneeUsername) {
   );
 }
 
+/**
+ * @description - This function formats the comment to be posted based on an array of multiple replacement objects.
+ * @param {Array} replacements - an array of replacement objects, each containing:
+ *  - {String} replacementString - the string to replace the placeholder in the md file
+ *  - {String} placeholderString - the placeholder to be replaced in the md file
+ * @param {String} filePathToFormat - the path of the md file to be formatted
+ * @param {String} textToFormat - the text to be formatted. If null, use the md file provided in the path. If provided, format that text
+ * @returns {String} - returns a formatted comment to be posted on github
+ */
+function formatComment({ replacements, filePathToFormat, textToFormat }, fs) {
+    let commentToPost = textToFormat === null ? fs.readFileSync(filePathToFormat).toString('utf-8') : textToFormat;
+    
+    for (const { replacementString, placeholderString } of replacements) {
+        commentToPost = commentToPost.replace(placeholderString, replacementString)
+    }
+
+    return commentToPost;
+}
+
 module.exports = checkComplexityEligibility;
