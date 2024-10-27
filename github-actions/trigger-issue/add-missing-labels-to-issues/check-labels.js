dbe0d94c877d1670d6b8a01994997304406197d9
Santiago <53095957+santisecco@users.noreply.github.com>
2024-10-27 18:28:54 -0300

    Update check-labels.js

--- github-actions/trigger-issue/add-missing-labels-to-issues/check-labels.js
+++ github-actions/trigger-issue/add-missing-labels-to-issues/check-labels.js
@@ -139,9 +139,6 @@ async function addLabels(labelsToAdd, currentLabels) {
 
 
 function testGetWins() {
-  const fileName = "_wins-data.json";
-  const [sha, content] = getWins(fileName);
-  console.log(content.length);
 }
 
 
