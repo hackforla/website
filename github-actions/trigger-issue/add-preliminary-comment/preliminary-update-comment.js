c2e266bb70a1fe4d476797e998efd594cbd72df3
Will Gillis <40799239+t-will-gillis@users.noreply.github.com>
2025-04-02 11:20:54 -0700

    Update preliminary-update-comment.js

--- github-actions/trigger-issue/add-preliminary-comment/preliminary-update-comment.js
+++ github-actions/trigger-issue/add-preliminary-comment/preliminary-update-comment.js
@@ -53,8 +53,7 @@ async function main({ g, c }, { shouldPost, issueNum }) {
     assignee = await getLatestAssignee();
 
     // Check if developer is allowed to work on this issue
-    // const isAdminOrMerge = await memberOfAdminOrMergeTeam();
-    const isAdminOrMerge = false;
+    const isAdminOrMerge = await memberOfAdminOrMergeTeam();
     const isAssignedToAnotherIssue = await assignedToAnotherIssue();
 
     // Check if developer is allowed to work on complexity level of the issue
