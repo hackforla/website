eb067e377671ae3f3e2aad5f852a02f839393be2
Will Gillis <40799239+t-will-gillis@users.noreply.github.com>
2025-06-09 20:18:33 -0700

    Update get-open-assigned-issues.js
    tweaks to getOpenAssignedIssues

--- github-actions/utils/get-open-assigned-issues.js
+++ github-actions/utils/get-open-assigned-issues.js
@@ -1,12 +1,12 @@
 /**
  * Function to get all repo issues that either are not assigned to a currentTeam member, or are assigned to 
  * inactive members so that leadership can be made aware that the issue does not have an active team member assigned.
- * @param {Object} currentTeam                 - currentTeam members
- * @param {Object} inactiveMemberOpenIssue     - inactive team members assigned to an open issue 
+ * @param {Object} currentTeam                 - currentTeam members (optional)
+ * @param {Object} inactiveMemberOpenIssue     - inactive team members assigned to an open issue (optional)
  * @return {Object} nonTeamMemberOpenIssue     - non-team members assigned to open issues
  * @return {Object} inactiveMemberOpenIssue    - inactive team members, all assignments to open issues
  */
-async function getOpenAssignedIssues(currentTeam = {}, inactiveMemberOpenIssue = {}) {
+async function getOpenAssignedIssues(github, context, currentTeam = {}, inactiveMemberOpenIssue = {}) {
   let nonTeamMemberOpenIssue = {};
   let pageNum = 1;
   let result = [];
@@ -14,9 +14,9 @@ async function getOpenAssignedIssues(currentTeam = {}, inactiveMemberOpenIssue =
   // Since Github only allows to fetch max 100 items per request, we need to 'flip' pages
   while (true) {
     // Fetch 100 items per each page (`pageNum`)
+    // https://docs.github.com/en/rest/issues/issues?apiVersion=2022-11-28#list-repository-issues
     const openIssues = await github.request('GET /repos/{owner}/{repo}/issues', {
-      // owner: context.repo.owner,
-      owner: 'hackforla',
+      owner: context.repo.owner,
       repo: context.repo.repo,
       assignee: '*',
       per_page: 100,
@@ -33,19 +33,20 @@ async function getOpenAssignedIssues(currentTeam = {}, inactiveMemberOpenIssue =
     }
   }
 
-  // For the list of all open issues, check whether there is an assignee who is not a current team member
+  // Loop through each result individually
   for (const contributorInfo of result) {
     let assignee = contributorInfo.assignee.login;
     let issueNum = contributorInfo.number;
-    // Check if the assignee is not on the currentTeam, then 
+    // Check if assignee is not a currentTeam member, then find their other open issues
+    // Else if assignee is on the inactiveMember list, find all of their open issues
     if (!(assignee in currentTeam)) {
-      if (assignee in inactiveMemberOpenIssue && !inactiveMemberOpenIssue[assignee].includes(issueNum)) {
-        inactiveMemberOpenIssue[assignee].push(issueNum);
-      } else {
-        nonTeamMemberOpenIssue[assignee] = issueNum;
-      }
+      (nonTeamMemberOpenIssue[assignee] ??= []).push(issueNum);
+    } else if (assignee in inactiveMemberOpenIssue) {
+      inactiveMemberOpenIssue[assignee].push(issueNum);
     }
   }
 
   return [nonTeamMemberOpenIssue, inactiveMemberOpenIssue]; 
 }
+
+module.exports = getOpenAssignedIssues;
