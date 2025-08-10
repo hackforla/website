df06991ad7f13445c2c34b656e705efda6114ae1
Will Gillis <40799239+t-will-gillis@users.noreply.github.com>
2025-08-09 22:19:53 -0700

    Update get-issue-comments.js

--- github-actions/activity-trigger/get-issue-comments.js
+++ github-actions/activity-trigger/get-issue-comments.js
@@ -3,11 +3,9 @@ tenDaysAgo.setDate(tenDaysAgo.getDate() - 10);
 
 
 async function getIssueComments(github, context, issueNum) {
-  console.log(`IssueNum: ${issueNum}`);
-  console.log(`typeof: ${typeof issueNum}`);
+
   const owner = context.repo.owner;
   const repo = context.repo.repo;
-  // let issueNum = issueNum;
 
   const query = `query($owner: String!, $repo: String!, $issueNum: Int!) {
     repository(owner: $owner, name: $repo) {
