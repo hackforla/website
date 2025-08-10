7b2e209245ea6b3aec5a5a08efaaf93660bccb61
t-will-gillis <40799239+t-will-gillis@users.noreply.github.com>
2025-08-09 21:14:18 -0700

    split csv for testing, add new cron

--- github-actions/activity-trigger/first-post-to-skills-issue.js
+++ github-actions/activity-trigger/first-post-to-skills-issue.js
@@ -30,7 +30,7 @@ async function firstPostToSkillsIssue({g, c}) {
 
 
     try {
-        const csvPath = 'github-actions/activity-trigger/member_activity_history_bot.csv';
+        const csvPath = 'github-actions/activity-trigger/member_activity_history_bot_1.csv';
         const csvContent = fs.readFileSync(csvPath, 'utf-8');
 
         // Parse CSV assuming
