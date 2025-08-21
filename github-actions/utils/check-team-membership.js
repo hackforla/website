f085132671d0e9f682b3f46b43ea633df82f75bb
Prince Haywood <praytoo52@icloud.com>
2025-08-21 09:16:56 -0700

    Refactor check-team-membership.js to use context.repo.org and fix parameters

--- github-actions/utils/check-team-membership.js
+++ github-actions/utils/check-team-membership.js
@@ -1,20 +1,12 @@
-/** 
-* @param {octokit} github - Octokit object used to access GitHub API 
-* @param {String} githubUsername - The GitHub username of the user whose membership is to be checked.
-* @param {String} team - The HFLA team the username's membership is checked against. Example: 'website-write'
-
-- Returns true or false depending on whether the username is found on the passed team, 404 means the user passed wasn't 
-found on the team passed. Any other type of error will be thrown.
-- Need read:org permission to use this function, the least permissive token which contains this is the TEAMS token.
-Lack of permission will result in a 403 error.
-- The method of obtaining the GitHub username will vary depending on the contents of the context object. See GitHub action 
-docs on printing context information into the log.
-*/
-
-async function isMemberOfTeam(github, githubUsername, team) {
+/**
+ * @param {Object} github - GitHub client
+ * @param {Object} context - context object from actions/github-script
+ */
+// Need read:org permission to use this
+async function isMemberOfTeam(github, context, team, username) {
     try {
         await github.rest.teams.getMembershipForUserInOrg({
-            org: 'hackforla',
+            org: context.repo.org,
             team_slug: team,
             username: githubUsername
         });
