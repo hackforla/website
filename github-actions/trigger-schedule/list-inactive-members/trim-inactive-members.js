4746d9285d31f14594891dc91ca4747df93a542a
t-will-gillis <339.colorado.dev@gmail.com>
2026-04-10 21:19:16 -0700

    additional cleanup fs.writeFileSync()

--- github-actions/trigger-schedule/list-inactive-members/trim-inactive-members.js
+++ github-actions/trigger-schedule/list-inactive-members/trim-inactive-members.js
@@ -215,11 +215,9 @@ function writeData(removedContributors, notifiedContributors, nonTeamOpenIssue, 
   const filepath = 'github-actions/utils/_data/inactive-members.json';
   const inactiveMemberLists = { removedContributors, notifiedContributors, nonTeamOpenIssue, inactiveOpenIssue };
 
-  fs.writeFileSync(filepath, JSON.stringify(inactiveMemberLists, null, 2), (err) => {
-    if (err) throw err;
-    console.log(`-`.repeat(60));
-    console.log("File `inactive-members.json` saved successfully!");
-   });
+  fs.writeFileSync(filepath, JSON.stringify(inactiveMemberLists, null, 2));
+  console.log(`-`.repeat(60));
+  console.log("File `inactive-members.json` saved successfully!");
 }
 
 module.exports = main;
